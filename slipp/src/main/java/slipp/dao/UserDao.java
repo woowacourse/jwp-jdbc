255c274184190af066f10e6bdc4c27feaffb96d5
DaejunBae <dpudpu11@gmail.com>
2019-10-15 16:04:54 +0900

    refactor: findByUserId jdbcTemplate 적용

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -3,11 +3,7 @@ package slipp.dao;
 import nextstep.jdbc.JdbcTemplate;
 import nextstep.jdbc.RowMapper;
 import slipp.domain.User;
-import slipp.support.db.ConnectionManager;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -50,35 +46,17 @@ public class UserDao {
         return jdbcTemplate.executeQuery(Collections.EMPTY_LIST, sql, rowMapper);
     }
 
-    public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
+    public User findByUserId(final String userId) throws SQLException {
+        final String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+        final List<String> params = List.of(userId);
 
-            rs = pstmt.executeQuery();
-
+        return jdbcTemplate.executeQuery(params, sql, rs -> {
             User user = null;
             if (rs.next()) {
                 user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
                         rs.getString("email"));
             }
-
             return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+        });
     }
 }
