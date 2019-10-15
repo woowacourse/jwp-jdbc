8235b43b0560c6edcf4c99bb8ee7716e8c914af3
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-10-15 16:38:16 +0900

    feat: add executeQuery()

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -4,20 +4,41 @@ import slipp.support.db.ConnectionManager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcTemplate {
-    public void updateQuery(JdbcQuery query) throws SQLException {
+    public void updateQuery(UpdateQuery query) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
         try {
             con = ConnectionManager.getConnection();
-            query.service(con);
+            query.update(con);
         } finally {
             if (pstmt != null) {
                 pstmt.close();
             }
 
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public <T> T executeQuery(ExecuteQuery<T> query) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            return query.execute(con);
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
             if (con != null) {
                 con.close();
             }
