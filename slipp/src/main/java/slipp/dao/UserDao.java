f1ee73ced4d48e839cec7c6e48c6094c9f48af95
김강민 <kangminals5610@gmail.com>
2019-10-15 13:31:35 +0900

    feat: implement userDao findAll method

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -60,8 +60,37 @@ public class UserDao {
     }
 
     public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        List<User> users = new ArrayList<>();
+        
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = "SELECT * FROM USERS";
+            pstmt = con.prepareStatement(sql);
+            ResultSet resultSet = pstmt.executeQuery();
+
+            while (resultSet.next()) {
+                User user = new User(
+                    resultSet.getString("userId"),
+                    resultSet.getString("password"),
+                    resultSet.getString("name"),
+                    resultSet.getString("email")
+                );
+
+                users.add(user);
+            }
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
+
+        return users;
     }
 
     public User findByUserId(String userId) throws SQLException {
