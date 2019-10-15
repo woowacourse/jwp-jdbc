e42aace2417a13a732a036cafdd57505f7d95ca1
wbluke <pwb0127@naver.com>
2019-10-15 14:12:55 +0900

    refactor: insert, findById에 try-with-resources 구문 적용

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -13,26 +13,16 @@ import java.util.List;
 public class UserDao {
 
     public void insert(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
             pstmt.setString(1, user.getUserId());
             pstmt.setString(2, user.getPassword());
             pstmt.setString(3, user.getName());
             pstmt.setString(4, user.getEmail());
 
             pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
         }
     }
 
@@ -73,34 +63,30 @@ public class UserDao {
     }
 
     public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
+        String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
+
+        try (Connection con = ConnectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
             pstmt.setString(1, userId);
 
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-
+            User user = executeQuery(pstmt);
             return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
+        }
+    }
+
+    private User executeQuery(final PreparedStatement pstmt) throws SQLException {
+        User user = null;
+
+        try (ResultSet rs = pstmt.executeQuery()) {
+            if (rs.next()) {
+                String retrievedUserId = rs.getString("userId");
+                String password = rs.getString("password");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+
+                user = new User(retrievedUserId, password, name, email);
             }
         }
+        return user;
     }
 }
