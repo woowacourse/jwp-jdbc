2227b51736d499d81301865800ea238bfc6342ea
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-10-15 15:07:32 +0900

    feat: create JdbcTemplate

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,4 +1,26 @@
 package nextstep.jdbc;
 
+import slipp.support.db.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class JdbcTemplate {
+    public void updateQuery(JdbcQuery query) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            query.service(con);
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
 }
