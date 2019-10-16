d58f7860e6b2e2c440944c738dc7db6ce6fd0f74
hyunsoojung <jhs695@naver.com>
2019-10-16 19:41:33 +0900

    refactor: findByUserId() 리팩토링

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -1,6 +1,7 @@
 package slipp.dao;
 
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.RowMapper;
 import slipp.domain.User;
 import slipp.support.db.ConnectionManager;
 
@@ -12,8 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
-    public static final JdbcTemplate JDBC_TEMPLATE = new JdbcTemplate(ConnectionManager.getDataSource());
-
+    public static final JdbcTemplate<User> JDBC_TEMPLATE = new JdbcTemplate<>(ConnectionManager.getDataSource());
 
     public void insert(User user) throws SQLException {
         JDBC_TEMPLATE.update("INSERT INTO USERS VALUES (?, ?, ?, ?)", user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
@@ -58,35 +58,14 @@ public class UserDao {
         }
     }
 
-    public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
-
-            rs = pstmt.executeQuery();
-
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-
-            return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+    public User findByUserId(String id) throws SQLException {
+        RowMapper<User> rowMapper = resultSet -> {
+            String userId = resultSet.getString("userId");
+            String password = resultSet.getString("password");
+            String name = resultSet.getString("name");
+            String email = resultSet.getString("email");
+            return new User(userId, password, name, email);
+        };
+        return JDBC_TEMPLATE.read(rowMapper, "SELECT userId, password, name, email FROM USERS WHERE userId = ?", id);
     }
 }
