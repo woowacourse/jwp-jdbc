2d717b61e19ac30704e0085005b94e09e4d40f65
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-10-16 17:33:48 +0900

    refactor: update UserDao applied SqlMapper

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -1,10 +1,10 @@
 package slipp.dao;
 
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.SqlMapper;
 import slipp.domain.User;
 import slipp.support.db.ConnectionManager;
 
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,43 +12,31 @@ import java.util.List;
 public class UserDao {
     public void insert(User user) throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(ConnectionManager.getConnection());
-        jdbcTemplate.updateQuery(con -> {
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+        SqlMapper sqlMapper = new SqlMapper("INSERT INTO USERS VALUES (?, ?, ?, ?)");
+        sqlMapper.addAttribute(user.getUserId())
+                .addAttribute(user.getPassword())
+                .addAttribute(user.getName())
+                .addAttribute(user.getEmail());
 
-            PreparedStatement pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
-
-            return pstmt;
-        });
+        jdbcTemplate.updateQuery(sqlMapper);
     }
 
     public void update(User user) throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(ConnectionManager.getConnection());
-        jdbcTemplate.updateQuery(con -> {
-            String sql = "UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?";
+        SqlMapper sqlMapper = new SqlMapper("UPDATE USERS SET password = ?, name = ?, email = ? WHERE userId = ?");
+        sqlMapper.addAttribute(user.getPassword())
+                .addAttribute(user.getName())
+                .addAttribute(user.getEmail())
+                .addAttribute(user.getUserId());
 
-            PreparedStatement pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getPassword());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getEmail());
-            pstmt.setString(4, user.getUserId());
-
-            return pstmt;
-        });
+        jdbcTemplate.updateQuery(sqlMapper);
     }
 
     public List<User> findAll() throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(ConnectionManager.getConnection());
+        SqlMapper sqlMapper = new SqlMapper("SELECT userId, password, name, email FROM USERS");
 
-        return jdbcTemplate.executeQuery(
-                con -> {
-                    String sql = "SELECT userId, password, name, email FROM USERS";
-                    PreparedStatement pstmt = con.prepareStatement(sql);
-                    return pstmt;
-                },
+        return jdbcTemplate.executeQuery(sqlMapper,
                 resultSet -> {
                     List<User> users = new ArrayList<>();
                     while (resultSet.next()) {
@@ -66,13 +54,10 @@ public class UserDao {
 
     public User findByUserId(String userId) throws SQLException {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(ConnectionManager.getConnection());
-        return jdbcTemplate.executeQuery(
-                con -> {
-                    String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-                    PreparedStatement pstmt = con.prepareStatement(sql);
-                    pstmt.setString(1, userId);
-                    return pstmt;
-                },
+        SqlMapper sqlMapper = new SqlMapper("SELECT userId, password, name, email FROM USERS WHERE userid=?");
+        sqlMapper.addAttribute(userId);
+
+        return jdbcTemplate.executeQuery(sqlMapper,
                 resultSet -> {
                     User user = null;
                     if (resultSet.next()) {
