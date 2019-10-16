fa78276adfbf9c5f1de9cbf54f427af1018fc1ee
MK S <conatuseus@gmail.com>
2019-10-16 13:49:18 +0900

    feat: JdbcTemplate 구현

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,4 +1,34 @@
 package nextstep.jdbc;
 
-public class JdbcTemplate {
+import slipp.domain.User;
+import slipp.support.db.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class JdbcTemplate {
+
+    public void update(User user) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = createQuery();
+            pstmt = con.prepareStatement(sql);
+            setValuesForUpdate(user, pstmt);
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public abstract String createQuery();
+
+    public abstract void setValuesForUpdate(User user, PreparedStatement pstmt) throws SQLException;
 }
