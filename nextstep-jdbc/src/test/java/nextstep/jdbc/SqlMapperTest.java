1365f8550e09d0c22554c3597be468bce5d06ca5
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-10-16 17:27:15 +0900

    test: SqlMapperTest

---
+++ nextstep-jdbc/src/test/java/nextstep/jdbc/SqlMapperTest.java
@@ -0,0 +1,32 @@
+package nextstep.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import slipp.domain.User;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SqlMapperTest {
+    private static final Logger logger = LoggerFactory.getLogger(SqlMapperTest.class);
+
+    @Test
+    void create() {
+        User user = new User("aiden", "1234", "aiden", "aiden@naver.com");
+        SqlMapper sqlMapper = new SqlMapper("INSERT INTO USERS VALUES (?, ?, ?, ?)");
+        sqlMapper.addAttribute(user.getUserId())
+                .addAttribute(user.getPassword())
+                .addAttribute(user.getName())
+                .addAttribute(user.getEmail());
+
+        SqlMapper expected = new SqlMapper("INSERT INTO USERS VALUES (?, ?, ?, ?)");
+        expected.addAttribute(user.getUserId())
+                .addAttribute(user.getPassword())
+                .addAttribute(user.getName())
+                .addAttribute(user.getEmail());
+
+        assertThat(sqlMapper).isEqualTo(expected);
+    }
+
+    // TODO: execute test
+}
