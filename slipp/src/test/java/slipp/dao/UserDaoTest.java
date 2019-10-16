72978df7aa9bf30aa7b8c5b704f0c0c0c204ca3d
wbluke <pwb0127@naver.com>
2019-10-16 13:18:10 +0900

    test: UserDaoTest에 예외 관련 테스트 추가

--- slipp/src/test/java/slipp/dao/UserDaoTest.java
+++ slipp/src/test/java/slipp/dao/UserDaoTest.java
@@ -1,30 +1,38 @@
 package slipp.dao;
 
+import nextstep.jdbc.exception.ExecuteUpdateFailedException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import slipp.domain.User;
 import slipp.dto.UserUpdatedDto;
+import slipp.exception.UserNotFoundException;
 import slipp.support.db.ConnectionManager;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserDaoTest {
+
+    private UserDao userDao;
+
     @BeforeEach
     public void setup() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("jwp.sql"));
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
+
+        userDao = new UserDao();
     }
 
     @Test
-    public void crud() throws Exception {
+    public void crud() {
         User expected = new User("userId", "password", "name", "javajigi@email.com");
-        UserDao userDao = new UserDao();
         userDao.insert(expected);
         User actual = userDao.findByUserId(expected.getUserId());
         assertThat(actual).isEqualTo(expected);
@@ -36,9 +44,23 @@ public class UserDaoTest {
     }
 
     @Test
-    public void findAll() throws Exception {
-        UserDao userDao = new UserDao();
+    public void findAll() {
         List<User> users = userDao.findAll();
         assertThat(users).hasSize(1);
+    }
+
+    @Test
+    @DisplayName("중복된 User를 저장하면 예외가 발생한다")
+    void addDuplicateUser() {
+        User user = new User("userId", "password", "name", "javajigi@email.com");
+
+        userDao.insert(user);
+        assertThrows(ExecuteUpdateFailedException.class, () -> userDao.insert(user));
+    }
+
+    @Test
+    @DisplayName("없는 User를 조회하면 예외가 발생한다")
+    void findByUserId_notFound() {
+        assertThrows(UserNotFoundException.class, () -> userDao.findByUserId("userId"));
     }
 }
