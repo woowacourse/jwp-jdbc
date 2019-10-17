006c2d720d555cfdbc8eb180a4f99907b2928314
MK S <conatuseus@gmail.com>
2019-10-17 14:32:18 +0900

    refactor: 중복 템플릿 통합

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -4,17 +4,104 @@ import slipp.support.db.ConnectionManager;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class JdbcTemplate {
+public class JdbcTemplate<T> {
 
-    public void update(String sql) throws SQLException {
+    public T queryForObject(String sql, PreparedStatementSetter pstmtSetter, RowMapper<T> rowMapper) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmtSetter.setValues(pstmt);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return rowMapper.mapRow(rs);
+            }
+            throw new SQLException();
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public List<T> query(String sql, PreparedStatementSetter pstmtSetter, RowMapper<T> rowMapper) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<T> result = new ArrayList<>();
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmtSetter.setValues(pstmt);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                result.add(rowMapper.mapRow(rs));
+            }
+
+            return result;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public List<T> query(String sql, RowMapper<T> rowMapper) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        List<T> result = new ArrayList<>();
+        try {
+            con = ConnectionManager.getConnection();
+            pstmt = con.prepareStatement(sql);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                result.add(rowMapper.mapRow(rs));
+            }
+
+            return result;
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (pstmt != null) {
+                pstmt.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    public void update(String sql, PreparedStatementSetter pstmtSetter) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
         try {
             con = ConnectionManager.getConnection();
             pstmt = con.prepareStatement(sql);
-            setValuesForUpdate(pstmt);
+            pstmtSetter.setValues(pstmt);
             pstmt.executeUpdate();
         } finally {
             if (pstmt != null) {
@@ -25,6 +112,4 @@ public abstract class JdbcTemplate {
             }
         }
     }
-
-    public abstract void setValuesForUpdate(PreparedStatement pstmt) throws SQLException;
 }
