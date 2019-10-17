7b29123ebcdaf6c50b848ce50a434dd6f0641ce7
김강민 <kangminals5610@gmail.com>
2019-10-17 11:00:21 +0900

    [refactor] change JdbcTemplate abstract class

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,4 +1,35 @@
 package nextstep.jdbc;
 
-public class JdbcTemplate {
+import slipp.domain.User;
+import slipp.support.db.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class JdbcTemplate {
+    public void insert(User user) throws SQLException {
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        try {
+            con = ConnectionManager.getConnection();
+            String sql = createSql();
+            pstmt = con.prepareStatement(sql);
+            setParameters(user, pstmt);
+
+            pstmt.executeUpdate();
+        } finally {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+
+            if (con != null) {
+                con.close();
+            }
+        }
+    }
+
+    protected abstract String createSql();
+
+    protected abstract void setParameters(User user, PreparedStatement preparedStatement);
 }
