7a07679fcc869e69d80b2eee3dd6c91bff936471
yk1028 <wongeunsong94@gmail.com>
2019-10-17 16:52:05 +0900

    refactor: UserDao에서 Varargs 사용하도록 수정

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -1,50 +1,42 @@
 package slipp.dao;
 
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.RowMapper;
 import slipp.domain.User;
 import slipp.support.db.DataBaseManager;
 
 import java.util.List;
 
 public class UserDao {
+    private static final RowMapper<User> userRowMapper =
+            resultSet -> new User(resultSet.getString("userId"),
+                    resultSet.getString("password"),
+                    resultSet.getString("name"),
+                    resultSet.getString("email"));
+
     private JdbcTemplate jdbcTemplate = DataBaseManager.getJdbcTemplate();
 
     public void insert(User user) {
         String query = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+
         jdbcTemplate.update(query, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 
     public void update(User user) {
         String query = "UPDATE users SET password=?, name=?, email=? WHERE userId=?";
 
-        jdbcTemplate.update(query, pstmt -> {
-            pstmt.setString(1, user.getPassword());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getEmail());
-            pstmt.setString(4, user.getUserId());
-        });
+        jdbcTemplate.update(query, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
     }
 
     public List<User> findAll() {
         String query = "SELECT * FROM users";
 
-        return jdbcTemplate.select(
-                query,
-                resultSet -> new User(resultSet.getString("userId"),
-                        resultSet.getString("password"),
-                        resultSet.getString("name"),
-                        resultSet.getString("email")));
+        return jdbcTemplate.select(query, userRowMapper);
     }
 
     public User findByUserId(String userId) {
         String query = "SELECT userId, password, name, email FROM users WHERE userId=?";
 
-        return jdbcTemplate.selectForObject(
-                query,
-                resultSet -> new User(resultSet.getString("userId"),
-                        resultSet.getString("password"),
-                        resultSet.getString("name"),
-                        resultSet.getString("email")),
-                userId);
+        return jdbcTemplate.selectForObject(query, userRowMapper, userId);
     }
 }
