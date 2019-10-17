3b9b39aa9f1ba58aef4b38c577f892888de83dda
heebong <heebg2@naver.com>
2019-10-17 22:28:14 +0900

    refactor: remove PreparedStatementMapping class
    - replace JdbcTemplate.mappingPreparedStatement()

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -19,36 +19,18 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void update(String sql) {
-        update(sql, pstmt -> {
-        });
-    }
-
     public void update(String sql, Object... objects) {
-        update(sql, pstmt -> mappingPreparedStatement(pstmt, objects));
-    }
-
-    private void update(String sql, PreparedStatementMapping consumer) {
         try (Connection con = getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
-            consumer.adjustTo(pstmt);
+            mappingPreparedStatement(pstmt, objects);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new DatabaseAccessException(e);
         }
     }
 
-    public <T> T query(String sql, ResultSetHandler<T> resultSetHandler) {
-        return query(sql, pstmt -> {
-        }, resultSetHandler);
-    }
-
-    public <T> T query(String sql, ResultSetHandler<T> resultSetHandler, Object... objects) {
-        return query(sql, pstmt -> mappingPreparedStatement(pstmt, objects), resultSetHandler);
-    }
-
-    private <T> T query(String sql, PreparedStatementMapping mapping, ResultSetHandler<T> resultSetHandler) {
+    private <T> T query(String sql, ResultSetHandler<T> resultSetHandler, Object... objects) {
         try (Connection con = getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
-            mapping.adjustTo(pstmt);
+            mappingPreparedStatement(pstmt, objects);
             try (ResultSet rs = pstmt.executeQuery()) {
                 return resultSetHandler.toObject(rs);
             }
@@ -57,36 +39,18 @@ public class JdbcTemplate {
         }
     }
 
-    public <T> Optional<T> queryForObject(String sql, ObjectMapper<T> objectMapper) {
-        return queryForObject(sql, pstmt -> {
-        }, objectMapper);
-    }
-
     public <T> Optional<T> queryForObject(String sql, ObjectMapper<T> objectMapper, Object... objects) {
-        return queryForObject(sql, pstmt -> mappingPreparedStatement(pstmt, objects), objectMapper);
+        return queryForObjects(sql, objectMapper, objects).stream().findFirst();
     }
 
-    private <T> Optional<T> queryForObject(String sql, PreparedStatementMapping mapping, ObjectMapper<T> objectMapper) {
-        return queryForObjects(sql, mapping, objectMapper).stream().findFirst();
-    }
-
-    public <T> List<T> queryForObjects(String sql, ObjectMapper<T> objectMapper) {
-        return queryForObjects(sql, pstmt -> {
-        }, objectMapper);
-    }
-
-    public <T> List<T> queryForObjects(String sql, ObjectMapper<T> objectMapper, Object... objects) {
-        return queryForObjects(sql, pstmt -> mappingPreparedStatement(pstmt, objects), objectMapper);
-    }
-
-    private <T> List<T> queryForObjects(String sql, PreparedStatementMapping mapping, ObjectMapper<T> objectMapper) {
-        return query(sql, mapping, rs -> {
+    public <T> List<T> queryForObjects(String sql, ObjectMapper<T> objectMapper, Object... sqlObjects) {
+        return query(sql, rs -> {
             List<T> objects = new ArrayList<>();
             while (rs.next()) {
                 objects.add(objectMapper.toObject(rs));
             }
             return objects;
-        });
+        }, sqlObjects);
     }
 
     private Connection getConnection() throws SQLException {
--- nextstep-jdbc/src/main/java/nextstep/jdbc/PreparedStatementMapping.java
+++
@@ -1,9 +0,0 @@
-package nextstep.jdbc;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface PreparedStatementMapping {
-    void adjustTo(PreparedStatement pstmt) throws SQLException;
-}
