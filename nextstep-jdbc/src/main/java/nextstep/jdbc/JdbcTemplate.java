bd1d114a9daf2149fce7048e24fff7134039b5d2
이영현 <leey9494@icloud.com>
2019-10-17 17:31:21 +0900

    refactor: Extract UserDao's duplicated code to JDBCTemplate

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,4 +1,48 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class JdbcTemplate {
+    private ConnectionManager cm;
+
+    public JdbcTemplate(DBConnection dbConnection) {
+        this.cm = new ConnectionManager(dbConnection);
+    }
+
+    public void execute(String sql, Object... objects) throws SQLException {
+        try (Connection con = cm.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+            setValues(pstmt, objects);
+            pstmt.executeUpdate();
+        }
+    }
+
+    public <T> T query(String sql, RowMapper<T> rm) {
+        try (Connection con = cm.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+            try (ResultSet rs = pstmt.executeQuery()) {
+                return rm.mapRow(rs);
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rm, Object... objects) {
+        try (Connection con = cm.getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
+            setValues(pstmt, objects);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                return rm.mapRow(rs);
+            }
+        } catch (SQLException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void setValues(PreparedStatement pstmt, Object[] objects) throws SQLException {
+        for (int i = 0; i < objects.length; i++) {
+            pstmt.setObject(i + 1, objects[i]);
+        }
+    }
 }
