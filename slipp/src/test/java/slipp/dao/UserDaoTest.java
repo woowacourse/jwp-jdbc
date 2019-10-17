a6991e02f95eb6384aa6efb574b8a56b09986146
Seongmo Jeon <oeeen3@gmail.com>
2019-10-18 00:42:24 +0900

    test: 예외 경우 테스트 케이스 추가

--- slipp/src/test/java/slipp/dao/UserDaoTest.java
+++ slipp/src/test/java/slipp/dao/UserDaoTest.java
@@ -1,5 +1,6 @@
 package slipp.dao;
 
+import nextstep.jdbc.JdbcTemplateException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -12,19 +13,24 @@ import slipp.support.db.ConnectionManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserDaoTest {
+    private UserDao userDao;
+    private User expected;
+
     @BeforeEach
     public void setup() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("jwp.sql"));
+        userDao = UserDao.getInstance();
+        expected = new User("userId", "password", "name", "javajigi@email.com");
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
     }
 
     @Test
     public void crud() {
-        User expected = new User("userId", "password", "name", "javajigi@email.com");
-        UserDao userDao = UserDao.getInstance();
         userDao.insert(expected);
         User actual = userDao.findByUserId(expected.getUserId());
         assertThat(actual).isEqualTo(expected);
@@ -33,6 +39,17 @@ public class UserDaoTest {
         userDao.update(expected);
         actual = userDao.findByUserId(expected.getUserId());
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void create_fail() {
+        userDao.insert(expected);
+        assertThrows(JdbcTemplateException.class, () -> userDao.insert(expected));
+    }
+
+    @Test
+    void read_fail() {
+        assertNull(userDao.findByUserId("NO_USER"));
     }
 
     @Test
