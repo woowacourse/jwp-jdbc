8cc8b430a7d05622050f51777cbd49c9ba18ba98
heebong <heebg2@naver.com>
2019-10-18 11:21:38 +0900

    refactor: add PreparedStatementMapping interface

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -20,17 +20,21 @@ public class JdbcTemplate {
     }
 
     public void update(String sql, Object... sqlArgs) {
+        update(sql, pstmt -> mappingPreparedStatement(pstmt, sqlArgs));
+    }
+
+    public void update(String sql, PreparedStatementMapping pstmtMapping) {
         try (Connection con = getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
-            mappingPreparedStatement(pstmt, sqlArgs);
+            pstmtMapping.adjustTo(pstmt);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             throw new DatabaseAccessException(e);
         }
     }
 
-    private <T> T query(String sql, ResultSetHandler<T> resultSetHandler, Object... sqlArgs) {
+    private <T> T query(String sql, ResultSetHandler<T> resultSetHandler, PreparedStatementMapping pstmtMapping) {
         try (Connection con = getConnection(); PreparedStatement pstmt = con.prepareStatement(sql)) {
-            mappingPreparedStatement(pstmt, sqlArgs);
+            pstmtMapping.adjustTo(pstmt);
             try (ResultSet rs = pstmt.executeQuery()) {
                 return resultSetHandler.toObject(rs);
             }
@@ -40,17 +44,25 @@ public class JdbcTemplate {
     }
 
     public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object... sqlArgs) {
-        return queryForObjects(sql, rowMapper, sqlArgs).stream().findFirst();
+        return queryForObject(sql, rowMapper, pstmt -> mappingPreparedStatement(pstmt, sqlArgs));
+    }
+
+    public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, PreparedStatementMapping pstmtMapping) {
+        return queryForObjects(sql, rowMapper, pstmtMapping).stream().findFirst();
     }
 
     public <T> List<T> queryForObjects(String sql, RowMapper<T> rowMapper, Object... sqlArgs) {
+        return queryForObjects(sql, rowMapper, pstmt -> mappingPreparedStatement(pstmt, sqlArgs));
+    }
+
+    public <T> List<T> queryForObjects(String sql, RowMapper<T> rowMapper, PreparedStatementMapping pstmtMapping) {
         return query(sql, rs -> {
             List<T> objects = new ArrayList<>();
             while (rs.next()) {
                 objects.add(rowMapper.toObject(rs));
             }
             return objects;
-        }, sqlArgs);
+        }, pstmtMapping);
     }
 
     private Connection getConnection() throws SQLException {
