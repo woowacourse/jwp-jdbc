effe00cad9928c9d9024aada7774882e9fda5392
oeeen <oeeen3@gmail.com>
2019-10-18 23:46:34 +0900

    [JDBC 라이브러리] 마틴 미션 제출합니다. (#21)
    * feat: UserDao#update, findAll 구현
    
    * refactor: UserDao에 try-with-resources문 적용
    
    * refactor: JdbcTemplate#executeQuery 추가
    
    * refactor: JdbcTemplateException 추가
    
    * refactor: UserDao#update 리팩토링
    
    * refactor: UserDao#findAll 리팩토링
    
    * refactor: UserDao#findByUserId 리팩토링
    
    * refactor: RowMapper 추가
    
    * refactor: RowMapper를 이용한 JdbcTemplate#query 추가
    
    * refactor: UserDao#insert 메소드 쿼리 개선
    
    * refactor: UserDao를 LazyHolder를 이용한 Singleton으로 변경
    
    * refactor: Controller에 UserDao 적용
    
    * feat: JdbcTemplate에 트랜잭션 기능 추가
    
    * refactor: 강제 형변환 제거
    
    * refactor: generic 타입 명시
    
    * test: 예외 경우 테스트 케이스 추가
    
    * refactor: 불필요한 코드 제거
    
    * refactor: 불필요한 코드 제거

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,4 +1,128 @@
 package nextstep.jdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class JdbcTemplate {
+    private static final Logger logger = LoggerFactory.getLogger(JdbcTemplate.class);
+
+    private static JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
+
+    private JdbcTemplate(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public static JdbcTemplate getInstance(DataSource dataSource) {
+        if (jdbcTemplate == null) {
+            return new JdbcTemplate(dataSource);
+        }
+        return jdbcTemplate;
+    }
+
+    public void executeQuery(String query, Object... objects) {
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement pstmt = createPreparedStatement(con, query, objects)) {
+            pstmt.executeUpdate();
+        } catch (Exception e) {
+            logger.error("Error occurred while executing Query", e);
+            throw new JdbcTemplateException(e);
+        }
+    }
+
+    public <T> List<T> query(String query, Class<T> clazz, Object... objects) {
+        List<T> results = new ArrayList<>();
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement pstmt = createPreparedStatement(con, query, objects);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                T t = getResult(rs, clazz);
+                results.add(t);
+            }
+        } catch (Exception e) {
+            logger.error("Error occurred while executing Query", e);
+            throw new JdbcTemplateException(e);
+        }
+        return results;
+    }
+
+    public <T> List<T> query(String query, RowMapper<T> rowMapper, Object... objects) {
+        List<T> results = new ArrayList<>();
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement pstmt = createPreparedStatement(con, query, objects);
+             ResultSet rs = pstmt.executeQuery()) {
+            while (rs.next()) {
+                T t = rowMapper.mapRow(rs);
+                results.add(t);
+            }
+        } catch (Exception e) {
+            logger.error("Error occurred while executing Query", e);
+            throw new JdbcTemplateException(e);
+        }
+        return results;
+    }
+
+    public <T> T queryForObject(String query, Class<T> clazz, Object... objects) {
+        T result = null;
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement pstmt = createPreparedStatement(con, query, objects);
+             ResultSet rs = pstmt.executeQuery()) {
+            if (rs.next()) {
+                result = getResult(rs, clazz);
+            }
+        } catch (Exception e) {
+            logger.error("Error occurred while executing Query", e);
+            throw new JdbcTemplateException(e);
+        }
+        return result;
+    }
+
+    public <T> T queryForObject(String query, RowMapper<T> rowMapper, Object... objects) {
+        T result = null;
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement pstmt = createPreparedStatement(con, query, objects);
+             ResultSet rs = pstmt.executeQuery()) {
+            if (rs.next()) {
+                result = rowMapper.mapRow(rs);
+            }
+        } catch (Exception e) {
+            logger.error("Error occurred while executing Query", e);
+            throw new JdbcTemplateException(e);
+        }
+        return result;
+    }
+
+    private PreparedStatement createPreparedStatement(Connection con, String sql, Object... objects) throws SQLException {
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        for (int i = 0; i < objects.length; i++) {
+            pstmt.setObject(i + 1, objects[i]);
+        }
+        return pstmt;
+    }
+
+    private <T> T getResult(ResultSet rs, Class<T> clazz) throws Exception {
+        Object instance = clazz.getDeclaredConstructor().newInstance();
+        Arrays.stream(clazz.getDeclaredFields()).forEach(field -> setField(rs, instance, field));
+        return clazz.cast(instance);
+    }
+
+    private void setField(ResultSet rs, Object instance, Field field) {
+        try {
+            field.setAccessible(true);
+            field.set(instance, rs.getObject(field.getName()));
+        } catch (IllegalAccessException | SQLException e) {
+            logger.error("Error occurred while setting Field", e);
+            throw new JdbcTemplateException(e);
+        }
+    }
 }
--- slipp/src/test/java/slipp/dao/UserDaoTest.java
+++ slipp/src/test/java/slipp/dao/UserDaoTest.java
@@ -1,5 +1,6 @@
 package slipp.dao;
 
+import nextstep.jdbc.JdbcTemplateException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -12,19 +13,24 @@ import slipp.support.db.ConnectionManager;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserDaoTest {
+    private UserDao userDao;
+    private User expected;
+
     @BeforeEach
     public void setup() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("jwp.sql"));
+        userDao = UserDao.getInstance();
+        expected = new User("userId", "password", "name", "javajigi@email.com");
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
     }
 
     @Test
-    public void crud() throws Exception {
-        User expected = new User("userId", "password", "name", "javajigi@email.com");
-        UserDao userDao = new UserDao();
+    public void crud() {
         userDao.insert(expected);
         User actual = userDao.findByUserId(expected.getUserId());
         assertThat(actual).isEqualTo(expected);
@@ -36,8 +42,19 @@ public class UserDaoTest {
     }
 
     @Test
-    public void findAll() throws Exception {
-        UserDao userDao = new UserDao();
+    void create_fail() {
+        userDao.insert(expected);
+        assertThrows(JdbcTemplateException.class, () -> userDao.insert(expected));
+    }
+
+    @Test
+    void read_fail() {
+        assertNull(userDao.findByUserId("NO_USER"));
+    }
+
+    @Test
+    public void findAll() {
+        UserDao userDao = UserDao.getInstance();
         List<User> users = userDao.findAll();
         assertThat(users).hasSize(1);
     }
