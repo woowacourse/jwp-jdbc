8a1dfee2c7203ba61cdba8850f77db9a77d40fb6
yunheehyeon <ok94life@daum.net>
2019-10-18 19:31:22 +0900

    feat : JdbcTemplate data update

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,4 +1,22 @@
 package nextstep.jdbc;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class JdbcTemplate {
+    private final DataSource dataSource;
+
+    public JdbcTemplate(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void update(String sql, PreparedStatementSetter setter) throws SQLException {
+        try (Connection con = dataSource.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            setter.values(pstmt);
+            pstmt.executeUpdate();
+        }
+    }
 }
