649178498a5df9eda307d7060b222fe1f1d5a8a4
aiden <cnk9109@gmail.com>
2019-10-20 16:02:44 +0900

    refactor: update PreparedStatementBuilder

--- nextstep-jdbc/src/main/java/nextstep/jdbc/query/PreparedStatementBuilder.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/query/PreparedStatementBuilder.java
@@ -1,33 +1,8 @@
 package nextstep.jdbc.query;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
-public class PreparedStatementBuilder {
-    private String query;
-    private List<String> attributes;
-
-    public PreparedStatementBuilder(String query) {
-        this.query = query;
-        this.attributes = new ArrayList<>();
-    }
-
-    public PreparedStatementBuilder addAttribute(String attribute) {
-        attributes.add(attribute);
-
-        return this;
-    }
-
-    public PreparedStatement create(Connection con) throws SQLException {
-        PreparedStatement pstmt = con.prepareStatement(query);
-        for (int i = 0; i < attributes.size(); i++) {
-            pstmt.setString(i + 1, attributes.get(i));
-        }
-
-        return pstmt;
-    }
+public interface PreparedStatementBuilder {
+    void build(PreparedStatement pstmt) throws SQLException;
 }
