957e6ab12b2a24ff3a97eab990b9ce4b30bc301e
DaejunBae <dpudpu11@gmail.com>
2019-10-20 19:34:41 +0900

    refactor: NamedParsedSql 상수 추출

--- nextstep-jdbc/src/main/java/nextstep/jdbc/NamedParsedSql.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/NamedParsedSql.java
@@ -7,16 +7,17 @@ import java.util.TreeMap;
 
 public class NamedParsedSql {
     private static final String DELIMITER = ":";
+    private static final String SUBSTITUTION = "?";
 
     private final String originSql;
     private final Map<Integer, Object> params = new TreeMap<>();
 
     NamedParsedSql(final String sql, final Map<String, Object> params) {
         final StringBuilder sb = new StringBuilder(sql);
-
         params.forEach((key, value) -> {
             final int start = sb.indexOf(DELIMITER + key);
-            sb.replace(start, start + key.length() + 1, "?");
+            final int end = start + key.length() + 1;
+            sb.replace(start, end, SUBSTITUTION);
             this.params.put(start, value);
         });
         this.originSql = sb.toString();
