c6426c16f6b92a17bf64c8897998a286c9138c4f
hyojaekim <hj00686@naver.com>
2019-10-20 18:24:29 +0900

    refactor: UserDao JdbcTemplate 필드 추가

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -11,36 +11,42 @@ import java.util.List;
 
 public class UserDao {
 
+    private JdbcTemplate<User> jdbcTemplate;
+
+    public UserDao() {
+        this.jdbcTemplate = new JdbcTemplate<>(ConnectionManager.getDataSource());
+    }
+
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void insert(User user) throws SQLException {
         String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-        JdbcTemplate<User> jdbcTemplate = new JdbcTemplate<>(ConnectionManager.getDataSource());
 
         jdbcTemplate.update(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 
     public void update(User user) throws SQLException {
         String sql = "UPDATE USERS SET password=?, email=?, name=? WHERE userId=?";
-        JdbcTemplate<User> jdbcTemplate = new JdbcTemplate<>(ConnectionManager.getDataSource());
 
         jdbcTemplate.update(sql, user.getPassword(), user.getEmail(), user.getName(), user.getUserId());
     }
 
     public List<User> findAll() throws SQLException {
         String sql = "SELECT * FROM USERS";
-        JdbcTemplate<User> jdbcTemplate = new JdbcTemplate<>(ConnectionManager.getDataSource());
 
         RowMapper<User> userRowMapper = rs ->
-            new User(rs.getString("userId"),
-                rs.getString("password"),
-                rs.getString("name"),
-                rs.getString("email"));
+                new User(rs.getString("userId"),
+                        rs.getString("password"),
+                        rs.getString("name"),
+                        rs.getString("email"));
 
         return jdbcTemplate.query(sql, userRowMapper);
     }
 
     public User findByUserId(String userId) throws SQLException {
         String sql = "SELECT userId, password, name, email FROM USERS WHERE userId=?";
-        JdbcTemplate<User> jdbcTemplate = new JdbcTemplate<>(ConnectionManager.getDataSource());
 
         PreparedStatementSetter preparedStatementSetter = pstmt -> pstmt.setString(1, userId);
         RowMapper<User> userRowMapper = rs ->
