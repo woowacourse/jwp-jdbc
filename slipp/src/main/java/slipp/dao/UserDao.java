5227250b942f2316a8311ba53e120aa4a1766752
kdg <kmdngyu@gmail.com>
2019-10-21 01:16:48 +0900

    refactor: 유저 매핑 중복 제거

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -3,6 +3,8 @@ package slipp.dao;
 import nextstep.jdbc.JdbcTemplate;
 import slipp.domain.User;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
@@ -14,12 +16,11 @@ public class UserDao {
 
     public void insert(User user) {
         String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-        jdbcTemplate.update(sql, pstmt -> {
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
-        });
+        jdbcTemplate.update(sql,
+                user.getUserId(),
+                user.getPassword(),
+                user.getName(),
+                user.getEmail());
     }
 
     public void update(User user) {
@@ -35,24 +36,25 @@ public class UserDao {
     public List<User> findAll() {
         String sql = "SELECT userId, password, name, email FROM USERS";
         return jdbcTemplate.query(sql,
-                resultSet -> new User(resultSet.getString("userId"),
-                        resultSet.getString("password"),
-                        resultSet.getString("name"),
-                        resultSet.getString("email")));
+                this::mappingUser);
     }
 
     public User findByUserId(String userId) {
         String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
         User user = jdbcTemplate.queryForObject(sql,
-                resultSet -> new User(resultSet.getString("userId"),
-                        resultSet.getString("password"),
-                        resultSet.getString("name"),
-                        resultSet.getString("email")),
-                pstmt -> pstmt.setString(1, userId));
+                this::mappingUser,
+                userId);
 
         if (user == null) {
             throw new NotFoundUserException();
         }
         return user;
+    }
+
+    private User mappingUser(ResultSet resultSet) throws SQLException {
+        return new User(resultSet.getString("userId"),
+                resultSet.getString("password"),
+                resultSet.getString("name"),
+                resultSet.getString("email"));
     }
 }
