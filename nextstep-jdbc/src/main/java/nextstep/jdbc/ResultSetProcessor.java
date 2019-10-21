1737987301b1991b0bdec72c743943e19b90ac0f
mrkwon <erased11@gmail.com>
2019-10-21 16:38:43 +0900

    style: 불필요한 클래스 삭제

--- nextstep-jdbc/src/main/java/nextstep/jdbc/ResultSetProcessor.java
+++
@@ -1,9 +0,0 @@
-package nextstep.jdbc;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface ResultSetProcessor<T> {
-    T process(ResultSet resultSet) throws SQLException;
-}
