a6a0536750538276c9ef028012b05af2b4109daa
green4469 <green4469@gmail.com>
2019-10-21 15:52:31 +0900

    refactor: fix into camel-case

--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -7,25 +7,25 @@ import nextstep.jdbc.JdbcTemplate;
 import slipp.domain.User;
 
 public class UserDao {
-    private final JdbcTemplate JDBC_TEMPLATE;
+    private final JdbcTemplate JdbcTemplate;
 
     public UserDao() {
-        this.JDBC_TEMPLATE = new JdbcTemplate();
+        this.JdbcTemplate = new JdbcTemplate();
     }
 
     public void insert(User user) {
         String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-        JDBC_TEMPLATE.updateTemplate(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
+        JdbcTemplate.updateTemplate(sql, user.getUserId(), user.getPassword(), user.getName(), user.getEmail());
     }
 
     public void update(User user) {
         String sql = "UPDATE USERS SET password=?, name=?, email=? WHERE userId=?";
-        JDBC_TEMPLATE.updateTemplate(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
+        JdbcTemplate.updateTemplate(sql, user.getPassword(), user.getName(), user.getEmail(), user.getUserId());
     }
 
     public List<User> findAll() {
         String sql = "SELECT userId, password, name, email FROM USERS";
-        return JDBC_TEMPLATE.selectTemplate(sql, (rs) -> {
+        return JdbcTemplate.selectTemplate(sql, (rs) -> {
             List<User> users = new ArrayList<>();
             while (rs.next()) {
                 User user = new User(rs.getString("userId"), rs.getString("password"),
@@ -38,7 +38,7 @@ public class UserDao {
 
     public User findByUserId(String userId) {
         String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-        return JDBC_TEMPLATE.selectTemplate(sql, (rs) -> {
+        return JdbcTemplate.selectTemplate(sql, (rs) -> {
             List<User> users = new ArrayList<>();
             if (rs.next()) {
                 User user = new User(rs.getString("userId"), rs.getString("password"),
