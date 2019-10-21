98f91052a22cc4669d5ccd2010e531bcbcd7a992
mrkwon <erased11@gmail.com>
2019-10-21 16:40:00 +0900

    feat: 어노테이션 추가

--- nextstep-jdbc/src/main/java/nextstep/jdbc/ExecuteCallback.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/ExecuteCallback.java
@@ -3,6 +3,7 @@ package nextstep.jdbc;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface ExecuteCallback<T> {
     T result(PreparedStatement preparedStatement) throws SQLException;
 }
