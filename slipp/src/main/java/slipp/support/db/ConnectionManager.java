922ecc1dc37d14249f270d4039a32be556a5d715
SunHo Van <vsh123@naver.com>
2019-10-22 12:53:37 +0900

    feat: dataSource 설정 외부 주입

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -1,31 +1,11 @@
 package slipp.support.db;
 
-import org.apache.commons.dbcp2.BasicDataSource;
-
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
 
 public class ConnectionManager {
-    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/fakebook?useUnicode=true&characterEncoding=utf8&serverTimezone=UTC";
-    private static final String DB_USERNAME = "jwp_blog";
-    private static final String DB_PW = "1234";
+    private static DataSource dataSource;
 
     public static DataSource getDataSource() {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
-        return ds;
-    }
-
-    public static Connection getConnection() {
-        try {
-            return getDataSource().getConnection();
-        } catch (SQLException e) {
-            throw new IllegalStateException(e);
-        }
+        return dataSource;
     }
 }
