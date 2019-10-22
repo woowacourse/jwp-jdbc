5bc7ad75b0539170cba8b47f5c45d6326c0819cf
wbluke <pwb0127@naver.com>
2019-10-22 13:24:44 +0900

    feat: ConnectionManager에서 db.properties를 읽어서 DataSource를 생성하도록 수정

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -1,21 +1,47 @@
 package slipp.support.db;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.lookup.DataSourceLookupFailureException;
 
 import javax.sql.DataSource;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
-    private static final String DB_USERNAME = "sa";
-    private static final String DB_PW = "";
+
+    private static final Logger log = LoggerFactory.getLogger(ConnectionManager.class);
+
+    private ConnectionManager() {
+    }
 
     public static DataSource getDataSource() {
+        Properties properties = getDBProperties();
+
+        String driverClassName = properties.getProperty("jdbc.driverClass");
+        String url = properties.getProperty("jdbc.url");
+        String userName = properties.getProperty("jdbc.username");
+        String password = properties.getProperty("jdbc.password");
+
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
+        ds.setDriverClassName(driverClassName);
+        ds.setUrl(url);
+        ds.setUsername(userName);
+        ds.setPassword(password);
         return ds;
+    }
+
+    private static Properties getDBProperties() {
+        try {
+            Properties properties = new Properties();
+            FileReader fileReader = new FileReader("src/main/resources/db.properties");
+            properties.load(fileReader);
+            return properties;
+        } catch (IOException e) {
+            log.debug("datasource creation failed : {}", e.getMessage());
+            throw new DataSourceLookupFailureException(e.getMessage(), e);
+        }
     }
 }
