6333fa33e3d46bd23a83b09e5d84cd4d497250d9
김강민 <kangminals5610@gmail.com>
2019-10-22 14:54:48 +0900

    feat : add excetption test in UserDaoTest

--- slipp/src/test/java/slipp/dao/UserDaoTest.java
+++ slipp/src/test/java/slipp/dao/UserDaoTest.java
@@ -1,6 +1,10 @@
 package slipp.dao;
 
 import nextstep.jdbc.ConnectionGenerator;
+import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.RowMapper;
+import nextstep.jdbc.exception.SelectQueryException;
+import nextstep.jdbc.exception.SqlUpdateException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -8,27 +12,32 @@ import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import slipp.domain.User;
 import slipp.dto.UserUpdatedDto;
+import slipp.exception.UserNotFoundException;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static slipp.dao.UserDao.DB_DRIVER;
 import static slipp.dao.UserDao.DB_PW;
 import static slipp.dao.UserDao.DB_URL;
 import static slipp.dao.UserDao.DB_USERNAME;
 
 public class UserDaoTest {
+    private UserDao userDao;
+
     @BeforeEach
     public void setup() {
         ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
         populator.addScript(new ClassPathResource("jwp.sql"));
         DatabasePopulatorUtils.execute(populator, ConnectionGenerator.getDataSource(DB_DRIVER, DB_URL, DB_USERNAME, DB_PW));
+        userDao = new UserDao();
     }
 
     @Test
     public void crud() {
         User expected = new User("userId", "password", "name", "javajigi@email.com");
-        UserDao userDao = new UserDao();
+
         userDao.insert(expected);
         User actual = userDao.findByUserId(expected.getUserId());
         assertThat(actual).isEqualTo(expected);
@@ -40,8 +49,26 @@ public class UserDaoTest {
     }
 
     @Test
+    void update_exception() {
+        User expected = new User(null, null, null, null);
+        assertThrows(SqlUpdateException.class, () -> userDao.insert(expected));
+    }
+
+    @Test
+    void select_exception() {
+        assertThrows(UserNotFoundException.class, () -> userDao.findByUserId("userId"));
+    }
+
+    @Test
+    void list_select_exception() {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(ConnectionGenerator.getDataSource(DB_DRIVER, DB_URL, DB_USERNAME, DB_PW));
+        User user = new User("userId", "password", "name", "javajigi@email.com");
+        RowMapper<User> rowMapper = rs -> user;
+        assertThrows(SelectQueryException.class, () -> jdbcTemplate.listQuery(null, rowMapper));
+    }
+
+    @Test
     public void findAll() {
-        UserDao userDao = new UserDao();
         List<User> users = userDao.findAll();
         assertThat(users).hasSize(1);
     }
