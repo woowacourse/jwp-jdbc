6f70130323bcc6980c34c9ab2dc1b9109d4339c3
yunheehyeon <ok94life@daum.net>
2019-10-23 15:24:57 +0900

    [JDBC 라이브러리] 유니 미션 제출합니다. (#42)
    * feat : user update, find all 구현
    
    * feat : JdbcTemplate data update
    
    * feat : JdbcTemplate queryForObject 추가
    
    * refactor : UserDao findAll jdbcTemplate 적용
    
    * refactor : list 조회 중복 제거
    
    * refactor : ConnectionManager nextstep-jdbc 이동
    
    * refactor : ResultSet close 자동 호출
    
    * refactor : update row 개수를 반환하도록 변경
    
    * refactor : SQLException -> RuntimeException
    
    * refactor : 가변인자로 값을 전달
    
    * refactor : JdbcTemplate에 ConnectionManager 주입

--- nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,4 +1,96 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JdbcTemplate {
+    private final ConnectionManager connectionManager;
+
+    public JdbcTemplate(ConnectionManager connectionManager) {
+        this.connectionManager = connectionManager;
+    }
+
+    public int update(String sql, PreparedStatementSetter setter) {
+        try (Connection con = connectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            setter.values(pstmt);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public int update(String sql, Object... objects) {
+        try (Connection con = connectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            setValues(pstmt, objects);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public <T> T queryForObject(String sql, PreparedStatementSetter setter, RowMapper<T> mapper) {
+        try (Connection con = connectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            setter.values(pstmt);
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            return mapper.mapRow(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> mapper, Object... objects) {
+        try (Connection con = connectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            setValues(pstmt, objects);
+            ResultSet rs = pstmt.executeQuery();
+            rs.next();
+            return mapper.mapRow(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public <T> List<T> query(String sql, PreparedStatementSetter setter, RowMapper<T> mapper) {
+        try (Connection con = connectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            setter.values(pstmt);
+            ResultSet rs = pstmt.executeQuery();
+            return getRows(rs, mapper);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> mapper, Object... objects) {
+        try (Connection con = connectionManager.getConnection();
+             PreparedStatement pstmt = con.prepareStatement(sql)) {
+            setValues(pstmt, objects);
+            ResultSet rs = pstmt.executeQuery();
+            return getRows(rs, mapper);
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    private void setValues(PreparedStatement pstmt, Object... objects) throws SQLException {
+        for (int i = 0; i < objects.length; i++) {
+            pstmt.setObject(i + 1, objects[i]);
+        }
+    }
+
+    private <T> List<T> getRows(ResultSet rs, RowMapper<T> mapper) throws SQLException {
+        List<T> rows = new ArrayList<>();
+        while (rs.next()) {
+            rows.add(mapper.mapRow(rs));
+        }
+        return rows;
+    }
 }
--- slipp/src/main/java/slipp/dao/UserDao.java
+++ slipp/src/main/java/slipp/dao/UserDao.java
@@ -1,78 +1,51 @@
 package slipp.dao;
 
+import nextstep.jdbc.ConnectionManager;
+import nextstep.jdbc.JdbcTemplate;
 import slipp.domain.User;
-import slipp.support.db.ConnectionManager;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserDao {
-    public void insert(User user) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, user.getUserId());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getName());
-            pstmt.setString(4, user.getEmail());
+    private final JdbcTemplate jdbcTemplate = new JdbcTemplate(new ConnectionManager("testdb.properties"));
 
-            pstmt.executeUpdate();
-        } finally {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-
-            if (con != null) {
-                con.close();
-            }
-        }
+    public void insert(User user) {
+        String insertSql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+        jdbcTemplate.update(insertSql, user.getUserId(),
+                user.getPassword(), user.getName(), user.getEmail());
     }
 
-    public void update(User user) throws SQLException {
-        // TODO 구현 필요함.
+    public void update(User user) {
+        String sql = "UPDATE USERS SET password = ?, name = ?, email = ? WHERE userid = ?";
+        jdbcTemplate.update(sql, pstmt -> {
+            pstmt.setString(1, user.getPassword());
+            pstmt.setString(2, user.getName());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setString(4, user.getUserId());
+        });
     }
 
-    public List<User> findAll() throws SQLException {
-        // TODO 구현 필요함.
-        return new ArrayList<User>();
+    public List<User> findAll() {
+        String sql = "SELECT * FROM USERS";
+
+        return jdbcTemplate.query(sql,
+                pstmt -> {
+                },
+                this::getUser);
     }
 
-    public User findByUserId(String userId) throws SQLException {
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            con = ConnectionManager.getConnection();
-            String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, userId);
+    public User findByUserId(String userId) {
+        String sql = "SELECT userId, password, name, email FROM USERS WHERE userid=?";
 
-            rs = pstmt.executeQuery();
+        return jdbcTemplate.queryForObject(sql,
+                pstmt -> pstmt.setString(1, userId),
+                this::getUser);
+    }
 
-            User user = null;
-            if (rs.next()) {
-                user = new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
-                        rs.getString("email"));
-            }
-
-            return user;
-        } finally {
-            if (rs != null) {
-                rs.close();
-            }
-            if (pstmt != null) {
-                pstmt.close();
-            }
-            if (con != null) {
-                con.close();
-            }
-        }
+    private User getUser(ResultSet rs) throws SQLException {
+        return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
+                rs.getString("email"));
     }
 }
