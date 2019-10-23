4ec67a7a01324725918aceceea83197a99c0576e
DaejunBae <dpudpu11@gmail.com>
2019-10-23 16:19:43 +0900

    refactor: TypeParser 중복 코드 템플릿으로 추출 후 제거

--- nextstep-jdbc/src/main/java/nextstep/jdbc/utils/TypeParser.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/utils/TypeParser.java
@@ -9,21 +9,19 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
 
-// todo 예외처리 때문에 람다 사용하면 코드가 더러워져서 메소드를 만들어줬지만, 예외처리를 위한 중복코드가 생기는 문제
 public enum TypeParser {
-    SHORT(List.of(Short.class, short.class), TypeParser::getShort),
-    INTEGER(List.of(Integer.class, int.class), TypeParser::getInteger),
-    LONG(List.of(Long.class, long.class), TypeParser::getLong),
-    BOOLEAN(List.of(Boolean.class, boolean.class), TypeParser::getBoolean),
-    FLOAT(List.of(Float.class, float.class), TypeParser::getFloat),
-    DOUBLE(List.of(Double.class, double.class), TypeParser::getDouble),
-    STRING(List.of(String.class), TypeParser::getString);
+    SHORT(List.of(Short.class, short.class), ResultSet::getShort),
+    INTEGER(List.of(Integer.class, int.class), ResultSet::getInt),
+    LONG(List.of(Long.class, long.class), ResultSet::getLong),
+    BOOLEAN(List.of(Boolean.class, boolean.class), ResultSet::getBoolean),
+    FLOAT(List.of(Float.class, float.class), ResultSet::getFloat),
+    DOUBLE(List.of(Double.class, double.class), ResultSet::getDouble),
+    STRING(List.of(String.class), ResultSet::getString);
 
     private static final Logger logger = LoggerFactory.getLogger(TypeParser.class);
 
-    public static Map<Class<?>, BiFunction<ResultSet, String, Object>> map = new HashMap<>();
+    public static Map<Class<?>, Parser> map = new HashMap<>();
 
     static {
         for (final TypeParser typeParser : TypeParser.values()) {
@@ -34,77 +32,28 @@ public enum TypeParser {
     }
 
     private final List<Class<?>> type;
-    private final BiFunction<ResultSet, String, Object> parser;
+    private final Parser parser;
 
-    TypeParser(final List<Class<?>> type, final BiFunction<ResultSet, String, Object> parser) {
+    TypeParser(final List<Class<?>> type, final Parser parser) {
         this.type = type;
         this.parser = parser;
     }
 
     public static Object parse(final ResultSet resultSet, final String name, final Class<?> type) {
-        return map.get(type).apply(resultSet, name);
+        return map.get(type).template(resultSet, name);
     }
 
-    private static String getString(final ResultSet resultSet, final String name) {
-        try {
-            return resultSet.getString(name);
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-            throw new DataAccessException(e);
+    @FunctionalInterface
+    interface Parser {
+        default Object template(final ResultSet rs, final String name) {
+            try {
+                return parse(rs, name);
+            } catch (SQLException e) {
+                logger.error(e.getMessage());
+                throw new DataAccessException(e);
+            }
         }
-    }
 
-    private static int getInteger(final ResultSet resultSet, final String name) {
-        try {
-            return resultSet.getInt(name);
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-            throw new DataAccessException(e);
-        }
-    }
-
-    private static long getLong(final ResultSet resultSet, final String name) {
-        try {
-            return resultSet.getLong(name);
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-            throw new DataAccessException(e);
-        }
-    }
-
-    private static boolean getBoolean(final ResultSet resultSet, final String name) {
-        try {
-            return resultSet.getBoolean(name);
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-            throw new DataAccessException(e);
-        }
-    }
-
-    private static short getShort(final ResultSet resultSet, final String name) {
-        try {
-            return resultSet.getShort(name);
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-            throw new DataAccessException(e);
-        }
-    }
-
-    private static double getDouble(final ResultSet resultSet, final String name) {
-        try {
-            return resultSet.getDouble(name);
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-            throw new DataAccessException(e);
-        }
-    }
-
-    private static float getFloat(final ResultSet resultSet, final String name) {
-        try {
-            return resultSet.getFloat(name);
-        } catch (SQLException e) {
-            logger.error(e.getMessage());
-            throw new DataAccessException(e);
-        }
+        Object parse(final ResultSet rs, final String name) throws SQLException;
     }
 }
