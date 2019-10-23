cd03c267501e428db134007fe2aab14c424ec9ce
이영현 <leey9494@icloud.com>
2019-10-23 16:09:20 +0900

    feat: setting db connection information in db.properties

--- nextstep-jdbc/src/main/java/nextstep/jdbc/DBConnection.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/DBConnection.java
@@ -1,6 +1,15 @@
 package nextstep.jdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class DBConnection {
+    private static final Logger log = LoggerFactory.getLogger(DBConnection.class);
+
     private String driver;
     private String url;
     private String userName;
@@ -13,8 +22,28 @@ public class DBConnection {
         this.password = password;
     }
 
+    public DBConnection() {
+        InputStream in = getClass().getClassLoader().getResourceAsStream("db.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(in);
+        } catch (IOException e) {
+            log.debug(e.getMessage());
+            throw new PropertiesLoadException(e);
+        }
+
+        driver = properties.getProperty("jdbc.driverClass");
+        url = properties.getProperty("jdbc.url");
+        userName = properties.getProperty("jdbc.username");
+        password = properties.getProperty("jdbc.password");
+    }
+
     public static DBConnection getConnection(String driver, String url, String userName, String password) {
         return new DBConnection(driver, url, userName, password);
+    }
+
+    public static DBConnection getConnection2() {
+        return new DBConnection();
     }
 
     public String getDriver() {
---
+++ slipp/src/test/java/nextstep/jdbc/DBConnectionTest.java
@@ -0,0 +1,34 @@
+package nextstep.jdbc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class DBConnectionTest {
+    private Properties properties;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        properties = new Properties();
+        String resource = "db.properties";
+        InputStream in = getClass().getClassLoader().getResourceAsStream(resource);
+        properties.load(in);
+    }
+
+    @Test
+    void get_properties_test() {
+        assertThat(properties.getProperty("jdbc.url")).isEqualTo("jdbc:mysql://localhost:13306/jwp_jdbc?useUnicode=true&characterEncoding=utf8");
+        assertThat(properties.getProperty("jdbc.driverClass")).isEqualTo("com.mysql.cj.jdbc.Driver");
+        assertThat(properties.getProperty("jdbc.username")).isEqualTo("yh");
+    }
+
+    @Test
+    void db_connection_test() {
+        assertThat(DBConnection.getConnection2()).isNotNull();
+    }
+}
