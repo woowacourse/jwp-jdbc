7751e84c2e0f84e1850aef8994d88f6c7ccd8698
wbluke <pwb0127@naver.com>
2019-10-24 14:08:09 +0900

    test: Years of Professional Coding Experience 쿼리 작성

--- study/src/test/java/sql/DatasetTest.java
+++ study/src/test/java/sql/DatasetTest.java
@@ -15,7 +15,7 @@ public class DatasetTest {
 
     @Test
     @DisplayName("Coding As A Hobby")
-    void findCodingAsHobby_timeout() {
+    void findCodingAsHobby() {
         String sql = "SELECT round((count(*) * 100 / (SELECT count(*) FROM survey_results_public)), 1) AS percent" +
                 " FROM survey_results_public" +
                 " WHERE hobby = 'Yes'";
@@ -23,5 +23,27 @@ public class DatasetTest {
         Object o = jdbcTemplate.executeQueryWithUniqueResult(sql, rs -> rs.getObject("percent"))
                 .orElseThrow(RuntimeException::new);
         assertThat((BigDecimal) o).isEqualTo("80.8");
+    }
+
+    @Test
+    @DisplayName("Years of Professional Coding Experience by Developer Type")
+    void findYearsOfProfessionalCoding() {
+        String sqlByEngineeringManager = createYearsOfProfQuery("Engineering manager");
+        Object o1 = jdbcTemplate.executeQueryWithUniqueResult(sqlByEngineeringManager, rs -> rs.getObject("avg"))
+                .orElseThrow(RuntimeException::new);
+        
+        assertThat((Double) o1).isEqualTo(10.2);
+
+        String sqlByDevOpsSpecialist = createYearsOfProfQuery("DevOps specialist");
+        Object o2 = jdbcTemplate.executeQueryWithUniqueResult(sqlByDevOpsSpecialist, rs -> rs.getObject("avg"))
+                .orElseThrow(RuntimeException::new);
+        
+        assertThat((Double) o2).isEqualTo(8.0);
+    }
+
+    private String createYearsOfProfQuery(final String devType) {
+        return "SELECT round(avg(if(YearsCodingProf = '30 or more years', 30, substring_index(YearsCodingProf, '-', 1))), 1) AS avg" +
+                    " FROM survey_results_public" +
+                    " WHERE DevType like '%" + devType + "%' AND YearsCodingProf != 'NA'";
     }
 }
