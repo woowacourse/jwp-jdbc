3f9f20fae58df41d6ff8a95ed9ec64e98362853c
이두호 <ddu0422@naver.com>
2019-10-24 14:29:52 +0900

    feat: add custom exception

--- slipp/src/main/java/slipp/support/db/exception/NotFoundProperties.java
+++ slipp/src/main/java/slipp/support/db/exception/NotFoundProperties.java
@@ -1,4 +1,9 @@
 package slipp.support.db.exception;
 
-public class NotFoundProperties extends Throwable {
+public class NotFoundProperties extends RuntimeException {
+    private static final String MESSAGE = "설정 파일을 찾을 수 없습니다.";
+
+    public NotFoundProperties() {
+        super(MESSAGE);
+    }
 }
