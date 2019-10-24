ffcffd9be5c7c3240bd8451e201f482b1d17b936
wbluke <pwb0127@naver.com>
2019-10-24 14:00:25 +0900

    test: Coding As A Hobby 쿼리 작성

--- study/src/test/java/sql/DatasetTest.java
+++ study/src/test/java/sql/DatasetTest.java
@@ -5,19 +5,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import slipp.support.db.ConnectionManager;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class DatasetTest {
 
-    public static final String TABLE_NAME = "survey_results_public";
     private final JdbcTemplate jdbcTemplate = new JdbcTemplate(ConnectionManager.getDataSource("src/test/java/sql/db.properties"));
 
     @Test
     @DisplayName("Coding As A Hobby")
-    void findCodingAsHobby() throws Exception {
-        Object o = jdbcTemplate.executeQueryWithUniqueResult("SELECT COUNT(*) FROM " + TABLE_NAME, rs -> rs.getObject("count(*)"))
-                .orElseThrow(RuntimeException::new);
+    void findCodingAsHobby_timeout() {
+        String sql = "SELECT round((count(*) * 100 / (SELECT count(*) FROM survey_results_public)), 1) AS percent" +
+                " FROM survey_results_public" +
+                " WHERE hobby = 'Yes'";
 
-        assertThat((long) o).isEqualTo(98855L);
+        Object o = jdbcTemplate.executeQueryWithUniqueResult(sql, rs -> rs.getObject("percent"))
+                .orElseThrow(RuntimeException::new);
+        assertThat((BigDecimal) o).isEqualTo("80.8");
     }
 }
