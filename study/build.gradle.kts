619b0f0151304194aa7debf4a2c8cdbb7e8cd957
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-10-25 17:36:58 +0900

    chore: add dependency to study module

--- study/build.gradle.kts
+++ study/build.gradle.kts
@@ -2,5 +2,10 @@ dependencies {
     implementation("org.reflections:reflections:0.9.11")
 
     api("com.fasterxml.jackson.core:jackson-databind:2.9.9.1")
+    api(project(":nextstep-jdbc"))
+    api(project(":slipp"))
+
     implementation("org.apache.ant:ant:1.10.6")
+    testImplementation("org.apache.commons:commons-dbcp2:2.6.0")
+    testImplementation("mysql:mysql-connector-java:8.0.17")
 }
