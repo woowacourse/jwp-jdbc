368b81e76987ce15d7e35d528081fd696faf650a
jinwook <jin942002@naver.com>
2019-10-25 17:29:13 +0900

    test: 쿼리 시간 계산하는 테스트 추가

--- study/src/test/java/sql/SqlTest.java
+++ study/src/test/java/sql/SqlTest.java
@@ -19,6 +19,34 @@ public class SqlTest {
     private JdbcTemplate jdbcTemplate;
     private DataBasePropertyReader dataBasePropertyReader;
 
+    private String hobbyQuery = "SELECT HOBBY, ROUND((COUNT(HOBBY) / (SELECT COUNT(*) FROM survey_results_public)) * 100,1) 'RATIO'" +
+            "FROM survey_results_public " +
+            "GROUP BY hobby;";
+
+    private String codingExperienceQuery = "select previous.job, round(avg(years),1) as coding_experience from (select\n" +
+            "  survey_results_public.Respondent,\n" +
+            "  SUBSTRING_INDEX(survey_results_public.YearsCodingProf,'-',1) as years,\n" +
+            "  SUBSTRING_INDEX(SUBSTRING_INDEX(survey_results_public.DevType,';', numbers.n), ';', -1) job\n" +
+            "from\n" +
+            "  (select 1 n union all\n" +
+            "   select 2 union all select 3 union all\n" +
+            "   select 4 union all select 5 union all\n" +
+            "   select 6 union all select 7 union all\n" +
+            "   select 8 union all select 9 union all\n" +
+            "   select 10 union all select 11 union all\n" +
+            "   select 12 union all select 13 union all\n" +
+            "   select 14 union all select 15 union all\n" +
+            "   select 16 union all select 17 union all\n" +
+            "   select 18 union all select 19\n" +
+            "   ) numbers INNER JOIN survey_results_public\n" +
+            "  on CHAR_LENGTH(survey_results_public.DevType)\n" +
+            "     -CHAR_LENGTH(REPLACE(survey_results_public.DevType, ';', ''))>=numbers.n-1\n" +
+            "     where survey_results_public.YearsCodingProf != 'NA'\n" +
+            "order by\n" +
+            "  Respondent, n) previous\n" +
+            "  group by previous.job\n" +
+            "  order by avg(years) desc;";
+
     private static final Logger logger = LoggerFactory.getLogger(SqlTest.class);
     @BeforeEach
     void setUp() {
@@ -32,10 +60,7 @@ public class SqlTest {
 
     @Test
     void hobby_설문결과() {
-        String sql = "SELECT HOBBY, ROUND((COUNT(HOBBY) / (SELECT COUNT(*) FROM survey_results_public)) * 100,1) 'RATIO'" +
-            "FROM survey_results_public " +
-            "GROUP BY hobby;";
-        List<HobbySurvey> hobbySurveys = jdbcTemplate.listQuery(sql,
+        List<HobbySurvey> hobbySurveys = jdbcTemplate.listQuery(hobbyQuery,
                 rs -> new HobbySurvey(rs.getString("Hobby"), rs.getFloat("ratio")));
         assertThat(hobbySurveys.get(0).getHobby()).isEqualTo("No");
         assertThat(hobbySurveys.get(0).getRatio()).isEqualTo(19.2f);
@@ -45,31 +70,8 @@ public class SqlTest {
 
     @Test
     void professional_coding_experience_설문결과() {
-        String sql = "select previous.job, round(avg(years),1) as coding_experience from (select\n" +
-                "  survey_results_public.Respondent,\n" +
-                "  SUBSTRING_INDEX(survey_results_public.YearsCodingProf,'-',1) as years,\n" +
-                "  SUBSTRING_INDEX(SUBSTRING_INDEX(survey_results_public.DevType,';', numbers.n), ';', -1) job\n" +
-                "from\n" +
-                "  (select 1 n union all\n" +
-                "   select 2 union all select 3 union all\n" +
-                "   select 4 union all select 5 union all\n" +
-                "   select 6 union all select 7 union all\n" +
-                "   select 8 union all select 9 union all\n" +
-                "   select 10 union all select 11 union all\n" +
-                "   select 12 union all select 13 union all\n" +
-                "   select 14 union all select 15 union all\n" +
-                "   select 16 union all select 17 union all\n" +
-                "   select 18 union all select 19\n" +
-                "   ) numbers INNER JOIN survey_results_public\n" +
-                "  on CHAR_LENGTH(survey_results_public.DevType)\n" +
-                "     -CHAR_LENGTH(REPLACE(survey_results_public.DevType, ';', ''))>=numbers.n-1\n" +
-                "     where survey_results_public.YearsCodingProf != 'NA'\n" +
-                "order by\n" +
-                "  Respondent, n) previous\n" +
-                "  group by previous.job\n" +
-                "  order by avg(years) desc;";
 
-        List<CodingExperienceServey> hobbySurveys = jdbcTemplate.listQuery(sql,
+        List<CodingExperienceServey> hobbySurveys = jdbcTemplate.listQuery(codingExperienceQuery,
                 rs -> new CodingExperienceServey(rs.getString("job"), rs.getFloat("coding_experience")));
         assertThat(hobbySurveys.get(0).getJob()).isEqualTo("Engineering manager");
         assertThat(hobbySurveys.get(0).getCodingExperience()).isEqualTo(10.2f);
@@ -79,18 +81,19 @@ public class SqlTest {
     }
 
     @Test
-    void connection() {
-        assertTimeout(Duration.ofMillis(1), () -> {
-            ConnectionGenerator.getConnection("com.mysql.cj.jdbc.Driver", "jdbc:mysql://localhost:13306/jwp_jdbc?useUnicode=true&characterEncoding=utf8",
-                    "techcourse", "password");
+    void hobby_쿼리_시간_계산() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            jdbcTemplate.listQuery(hobbyQuery,
+                    rs -> new HobbySurvey(rs.getString("Hobby"), rs.getFloat("ratio")));
         });
     }
 
     @Test
-    void dataSource() {
-        assertTimeout(Duration.ofMillis(1), () -> {
-            ConnectionGenerator.getDataSource("com.mysql.cj.jdbc.Driver", "jdbc:mysql://localhost:13306/jwp_jdbc?useUnicode=true&characterEncoding=utf8",
-                    "techcourse", "password").getConnection();
+    void codingExperience_쿼리_시간_계산() {
+
+        assertTimeout(Duration.ofMillis(100), () -> {
+            jdbcTemplate.listQuery(codingExperienceQuery,
+                    rs -> new CodingExperienceServey(rs.getString("job"), rs.getFloat("coding_experience")));
         });
     }
 }
