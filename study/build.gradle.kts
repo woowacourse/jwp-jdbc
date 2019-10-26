20c1eac8752804fea876ad5f72b001876b8b0135
yk1028 <wongeunsong94@gmail.com>
2019-10-26 16:44:13 +0900

    feat: 대용량 데이터 처리 테스트

--- study/build.gradle.kts
+++ study/build.gradle.kts
@@ -1,6 +1,12 @@
 dependencies {
+    api(project(":nextstep-jdbc"))
+
     implementation("org.reflections:reflections:0.9.11")
 
     api("com.fasterxml.jackson.core:jackson-databind:2.9.9.1")
     implementation("org.apache.ant:ant:1.10.6")
+
+    testImplementation("org.apache.commons:commons-dbcp2:2.6.0")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.5.1")
+    testImplementation("mysql:mysql-connector-java:8.0.17")
 }
