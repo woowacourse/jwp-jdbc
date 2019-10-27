aad8d42570c3df1292fdfc876ead5f0703a77380
yk1028 <wongeunsong94@gmail.com>
2019-10-27 16:28:23 +0900

    refactor: query 수정

--- study/src/test/java/sql/SqlTest.java
+++ study/src/test/java/sql/SqlTest.java
@@ -18,6 +18,29 @@ import java.util.Properties;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SqlTest {
+
+    private class Result {
+        private String name;
+        private double result;
+
+        Result(String name, double result) {
+            this.name = name;
+            this.result = result;
+        }
+    }
+
+    private class Results {
+        private Map<String, Double> results = new HashMap<>();
+
+        Results(List<Result> results) {
+            results.forEach(result -> this.results.put(result.name, result.result));
+        }
+
+        Double getResult(String name) {
+            return results.get(name);
+        }
+    }
+
     private static final String DB_PROPERTIES_PATH = "./src/test/resources/db.properties";
     private static final String DRIVER_CLASS_KEY = "jdbc.driverClass";
     private static final String URL_KEY = "jdbc.url";
@@ -54,28 +77,6 @@ public class SqlTest {
         }
     }
 
-    private class Result {
-        private String name;
-        private double result;
-
-        Result(String name, double result) {
-            this.name = name;
-            this.result = result;
-        }
-    }
-
-    private class Results {
-        private Map<String, Double> results = new HashMap<>();
-
-        Results(List<Result> results) {
-            results.forEach(result -> this.results.put(result.name, result.result));
-        }
-
-        Double getResult(String name) {
-            return results.get(name);
-        }
-    }
-
     @Test
     @DisplayName("Coding As a Hobby")
     void first() {
@@ -96,50 +97,49 @@ public class SqlTest {
     @Test
     @DisplayName("Years of Professional Coding Experience by Developer Type")
     void second() {
-        String query =
-                "SELECT DevType, ROUND(avg(year), 1) " +
-                        "FROM (" +
-                        "SELECT\n" +
-                        "  SUBSTRING_INDEX(SUBSTRING_INDEX(t1.DevType, ';', numbers.n), ';', -1) DevType,\n" +
-                        "  CASE\n" +
-                        "WHEN YearsCodingProf = '0-2 years' THEN 0\n" +
-                        "WHEN YearsCodingProf = '3-5 years' THEN 3\n" +
-                        "WHEN YearsCodingProf = '6-8 years' THEN 6\n" +
-                        "WHEN YearsCodingProf = '9-11 years' THEN 9\n" +
-                        "WHEN YearsCodingProf = '12-14 years' THEN 12\n" +
-                        "WHEN YearsCodingProf = '15-17 years' THEN 15\n" +
-                        "WHEN YearsCodingProf = '18-20 years' THEN 18\n" +
-                        "WHEN YearsCodingProf = '21-23 years' THEN 21\n" +
-                        "WHEN YearsCodingProf = '24-26 years' THEN 24\n" +
-                        "WHEN YearsCodingProf = '27-29 years' THEN 27\n" +
-                        "WHEN YearsCodingProf = '30 or more years' THEN 30\n" +
-                        "ELSE 0\n" +
-                        "END AS year\n" +
-                        "FROM\n" +
-                        "  (select 1 n union all\n" +
-                        "   select 2 union all\n" +
-                        "   select 3 union all\n" +
-                        "   select 4 union all \n" +
-                        "   select 5 union all \n" +
-                        "   select 6 union all \n" +
-                        "   select 7 union all \n" +
-                        "   select 8 union all \n" +
-                        "   select 9 union all \n" +
-                        "   select 10 union all \n" +
-                        "   select 11 union all \n" +
-                        "   select 12 union all \n" +
-                        "   select 13 union all \n" +
-                        "   select 14 union all \n" +
-                        "   select 15 union all \n" +
-                        "   select 16 union all \n" +
-                        "   select 17 union all \n" +
-                        "   select 18 union all \n" +
-                        "   select 19 union all \n" +
-                        "   select 20 ) numbers\n" +
-                        "   INNER JOIN survey_results_public t1\n" +
-                        "  ON CHAR_LENGTH(t1.DevType) - CHAR_LENGTH(REPLACE(t1.DevType, ';', '')) >= numbers.n - 1\n" +
-                        "  WHERE t1.DevType != 'NA' AND t1.YearsCodingProf != 'NA') sub\n" +
-                        "  GROUP BY DevType;";
+        String query = "SELECT DevType, ROUND(avg(year), 1) " +
+                "FROM (" +
+                "SELECT\n" +
+                "  SUBSTRING_INDEX(SUBSTRING_INDEX(t1.DevType, ';', numbers.n), ';', -1) DevType,\n" +
+                "  CASE\n" +
+                "WHEN YearsCodingProf = '0-2 years' THEN 0\n" +
+                "WHEN YearsCodingProf = '3-5 years' THEN 3\n" +
+                "WHEN YearsCodingProf = '6-8 years' THEN 6\n" +
+                "WHEN YearsCodingProf = '9-11 years' THEN 9\n" +
+                "WHEN YearsCodingProf = '12-14 years' THEN 12\n" +
+                "WHEN YearsCodingProf = '15-17 years' THEN 15\n" +
+                "WHEN YearsCodingProf = '18-20 years' THEN 18\n" +
+                "WHEN YearsCodingProf = '21-23 years' THEN 21\n" +
+                "WHEN YearsCodingProf = '24-26 years' THEN 24\n" +
+                "WHEN YearsCodingProf = '27-29 years' THEN 27\n" +
+                "WHEN YearsCodingProf = '30 or more years' THEN 30\n" +
+                "ELSE 0\n" +
+                "END AS year\n" +
+                "FROM\n" +
+                "  (select 1 n union all\n" +
+                "   select 2 union all\n" +
+                "   select 3 union all\n" +
+                "   select 4 union all \n" +
+                "   select 5 union all \n" +
+                "   select 6 union all \n" +
+                "   select 7 union all \n" +
+                "   select 8 union all \n" +
+                "   select 9 union all \n" +
+                "   select 10 union all \n" +
+                "   select 11 union all \n" +
+                "   select 12 union all \n" +
+                "   select 13 union all \n" +
+                "   select 14 union all \n" +
+                "   select 15 union all \n" +
+                "   select 16 union all \n" +
+                "   select 17 union all \n" +
+                "   select 18 union all \n" +
+                "   select 19 union all \n" +
+                "   select 20 ) numbers\n" +
+                "   INNER JOIN survey_results_public t1\n" +
+                "  ON numbers.n <= CHAR_LENGTH(t1.DevType) - CHAR_LENGTH(REPLACE(t1.DevType, ';', '')) + 1\n" +
+                "  WHERE t1.DevType != 'NA' AND t1.YearsCodingProf != 'NA') sub\n" +
+                "  GROUP BY DevType;";
 
         List<Result> res = jdbcTemplate.select(query, resultSet ->
                 new Result(resultSet.getString(1), resultSet.getDouble(2)));
