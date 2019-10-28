c5016b096fd6e9e4a0a3fdf81eb8be21f175cc09
hyojaekim <hj00686@naver.com>
2019-10-28 21:42:22 +0900

    feat: Properties를 활용한 DataSource 생성하는 기능 추가

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -3,6 +3,10 @@ package slipp.support.db;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public class ConnectionManager {
     private static final String DB_DRIVER = "org.h2.Driver";
@@ -11,11 +15,37 @@ public class ConnectionManager {
     private static final String DB_PW = "";
 
     public static DataSource getDataSource() {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
-        return ds;
+        return getDataSource(DB_DRIVER, DB_URL, DB_USERNAME, DB_PW);
+    }
+
+    public static DataSource getDataSource(Properties properties) {
+        return getDataSource(properties.getProperty("jdbc.driverClass"),
+                properties.getProperty("jdbc.url"),
+                properties.getProperty("jdbc.username"),
+                properties.getProperty("jdbc.password")
+        );
+    }
+
+    public static DataSource getDataSource(String driverClass, String url, String userName, String password) {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    public static Properties getProperties() {
+        try {
+            FileInputStream fileInputStream = new FileInputStream("../slipp/src/main/resources/db.properties");
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+            Properties properties = new Properties();
+            properties.load(bufferedInputStream);
+
+            return properties;
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException();
+        }
     }
 }
