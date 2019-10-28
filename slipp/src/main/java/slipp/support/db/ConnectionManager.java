cbb81940fc6bdbac5150a596347da2fe3a787da2
Seongmo Jeon <oeeen3@gmail.com>
2019-10-28 17:49:08 +0900

    feat: 대용량 데이터 처리 구현

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -3,21 +3,28 @@ package slipp.support.db;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
-    private static final String DB_USERNAME = "sa";
-    private static final String DB_PW = "";
+    private static final String resource = "db.properties";
+    private static final Properties properties = new Properties();
 
     public static DataSource getDataSource() {
+        InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(resource);
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
+        ds.setDriverClassName(properties.getProperty("jdbc.driverClass"));
+        ds.setUrl(properties.getProperty("jdbc.url"));
+        ds.setUsername(properties.getProperty("jdbc.username"));
+        ds.setPassword(properties.getProperty("jdbc.password"));
         return ds;
     }
 
