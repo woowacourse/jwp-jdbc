f182945e9a82610193543963998c43005e655632
aiden <cnk9109@gmail.com>
2019-10-29 13:06:47 +0900

    feat: update ConnectionManager using properties

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -1,23 +1,52 @@
 package slipp.support.db;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
-    private static final String DB_USERNAME = "sa";
-    private static final String DB_PW = "";
+    private static final Logger logger = LoggerFactory.getLogger(ConnectionManager.class);
+
+    private String DB_DRIVER;
+    private String DB_URL;
+    private String DB_USERNAME;
+    private String DB_PW;
+
+    private ConnectionManager() {
+        try {
+            String resource = "D:\\woowaBros\\level_3\\jwp-jdbc\\slipp\\src\\main\\resources\\db.properties";
+            Properties properties = new Properties();
+            FileInputStream fis = new FileInputStream(resource);
+            properties.load(new java.io.BufferedInputStream(fis));
+
+            DB_DRIVER = properties.getProperty("jdbc.driverClass");
+            DB_URL = properties.getProperty("jdbc.url");
+            DB_USERNAME = properties.getProperty("jdbc.username");
+            DB_PW = properties.getProperty("jdbc.password");
+        } catch (IOException e) {
+            logger.error("()", e.getCause());
+        }
+    }
+
+    public static ConnectionManager getInstance() {
+        return ConnectionManagerHolder.instance;
+    }
 
     public static DataSource getDataSource() {
+        ConnectionManager connectionManager = getInstance();
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
+        ds.setDriverClassName(connectionManager.DB_DRIVER);
+        ds.setUrl(connectionManager.DB_URL);
+        ds.setUsername(connectionManager.DB_USERNAME);
+        ds.setPassword(connectionManager.DB_PW);
+
         return ds;
     }
 
@@ -27,5 +56,9 @@ public class ConnectionManager {
         } catch (SQLException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    private static class ConnectionManagerHolder {
+        static ConnectionManager instance = new ConnectionManager();
     }
 }
