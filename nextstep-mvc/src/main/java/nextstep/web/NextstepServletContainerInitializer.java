1e4f59ad1f4cce8f1ee60af81aa6eb927a2adec2
강연욱 <fiasboc23@gmail.com>
2019-10-30 16:09:21 +0900

    feat: use db.properties

--- nextstep-mvc/src/main/java/nextstep/web/NextstepServletContainerInitializer.java
+++ nextstep-mvc/src/main/java/nextstep/web/NextstepServletContainerInitializer.java
@@ -1,15 +1,21 @@
 package nextstep.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HandlesTypes;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 @HandlesTypes(WebApplicationInitializer.class)
 public class NextstepServletContainerInitializer implements ServletContainerInitializer {
+    private static final Logger logger = LoggerFactory.getLogger(NextstepServletContainerInitializer.class);
+
     @Override
     public void onStartup(Set<Class<?>> webAppInitializerClasses, ServletContext servletContext)
             throws ServletException {
@@ -31,7 +37,12 @@ public class NextstepServletContainerInitializer implements ServletContainerInit
         }
 
         for (WebApplicationInitializer initializer : initializers) {
-            initializer.onStartup(servletContext);
+            try {
+                initializer.onStartup(servletContext);
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+                throw new IllegalArgumentException(e);
+            }
         }
     }
 }
--- nextstep-mvc/src/main/java/nextstep/web/WebApplicationInitializer.java
+++ nextstep-mvc/src/main/java/nextstep/web/WebApplicationInitializer.java
@@ -2,7 +2,8 @@ package nextstep.web;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import java.io.IOException;
 
 public interface WebApplicationInitializer {
-    void onStartup(ServletContext servletContext) throws ServletException;
+    void onStartup(ServletContext servletContext) throws ServletException, IOException;
 }
