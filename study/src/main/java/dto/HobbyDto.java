389a443669ffdf7928028380782e8fefa80a5f99
ep1stas1s <masloz@konkuk.ac.kr>
2019-10-31 01:44:29 +0900

    refactor: rename & remove constructor

--- study/src/main/java/dto/HobbyDto.java
+++ study/src/main/java/dto/HobbyDto.java
@@ -1,13 +1,8 @@
 package dto;
 
 public class HobbyDto {
-    private final String hobby;
-    private final double ratio;
-
-    public HobbyDto(String hobby, double ratio) {
-        this.hobby = hobby;
-        this.ratio = ratio;
-    }
+    private String hobby;
+    private double ratio;
 
     public String getHobby() {
         return hobby;
