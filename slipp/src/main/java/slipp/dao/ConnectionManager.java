485637c4baacfb3be59ef664bbde40d1f8e632ac
CODEMCD <psbum1609@gmail.com>
2019-11-02 18:51:09 +0900

    feat: properties 읽은 후 DataSource 생성하는 메서드 구현

--- slipp/src/main/java/slipp/dao/ConnectionManager.java
+++ slipp/src/main/java/slipp/dao/ConnectionManager.java
@@ -1,12 +1,19 @@
 package slipp.dao;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
+    private static final Logger log = LoggerFactory.getLogger(ConnectionManager.class);
+
     private static final String DB_DRIVER = "org.h2.Driver";
     private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
     private static final String DB_USERNAME = "sa";
@@ -19,6 +26,23 @@ public class ConnectionManager {
         ds.setUsername(DB_USERNAME);
         ds.setPassword(DB_PW);
         return ds;
+    }
+
+    public static DataSource getDataSource(String path) {
+        Properties properties = new Properties();
+        try (FileReader resources = new FileReader(path)) {
+            properties.load(resources);
+
+            BasicDataSource ds = new BasicDataSource();
+            ds.setDriverClassName(properties.getProperty("jdbc.driverClass"));
+            ds.setUrl(properties.getProperty("jdbc.url"));
+            ds.setUsername(properties.getProperty("jdbc.username"));
+            ds.setPassword(properties.getProperty("jdbc.password"));
+            return ds;
+        } catch (IOException e) {
+            log.error("Fail get datasource", e);
+            throw new IllegalArgumentException();
+        }
     }
 
     public static Connection getConnection() {
