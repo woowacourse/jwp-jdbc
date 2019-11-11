bda279802a6b12b12e7a9a07e03f1ef4418db27e
deocksoo <dusol92@snu.ac.kr>
2019-11-11 21:46:51 +0900

    refactor: ConnectionManager에서 설정 정보 db.properties로 분리

---
+++ slipp/src/main/java/slipp/support/db/exception/PropertiesLoadFailException.java
@@ -0,0 +1,7 @@
+package slipp.support.db.exception;
+
+public class PropertiesLoadFailException extends RuntimeException {
+    public PropertiesLoadFailException(Throwable e) {
+        super(e);
+    }
+}
