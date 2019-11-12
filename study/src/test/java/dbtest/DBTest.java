8319cf9ae21f5a761536c9475dc294a49f2b5692
yunheehyeon <ok94life@daum.net>
2019-11-12 16:00:49 +0900

    refactor : test 수정

--- study/src/test/java/dbtest/DBTest.java
+++ study/src/test/java/dbtest/DBTest.java
@@ -1,17 +1,13 @@
 package dbtest;
 
-import com.google.common.collect.Maps;
 import nextstep.jdbc.ConnectionManager;
 import nextstep.jdbc.JdbcTemplate;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 public class DBTest {
     private JdbcTemplate jdbcTemplate = new JdbcTemplate(new ConnectionManager("db.properties"));
@@ -20,12 +16,10 @@ public class DBTest {
     @Test
     void infoCodingAsHobby() {
         String sql = "SELECT hobby, count(hobby) / (SELECT count(hobby) FROM jwp_jdbc.survey_results_public) from jwp_jdbc.survey_results_public group by hobby;";
-        assertTimeout(Duration.ofMillis(100), () ->
-                jdbcTemplate.query(sql, rs -> "hobby : " + rs.getString(1) + ", rate : " + rs.getDouble(2))
-        );
 
-        Maps.newConcurrentMap();
-
+        for (String result : jdbcTemplate.query(sql, rs -> "hobby : " + rs.getString(1) + ", rate : " + rs.getDouble(2))) {
+            log.info(result);
+        }
     }
 
     @Test
@@ -37,10 +31,6 @@ public class DBTest {
                 "WHERE s.DevType LIKE CONCAT('%', d.name, '%') and s.YearsCodingProf != 'NA'\n" +
                 "GROUP BY d.name\n" +
                 "ORDER BY exp DESC;";
-
-        assertTimeout(Duration.ofMillis(3000), () ->
-                jdbcTemplate.query(sql, rs -> "Developer Type : " + rs.getString(1) + ", Years of Professional : " + rs.getDouble(2))
-        );
 
         for (String result : jdbcTemplate.query(sql, rs -> rs.getString(1) + ": " + rs.getDouble(2))) {
             log.info(result);
