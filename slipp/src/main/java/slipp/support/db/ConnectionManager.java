9663a86edfe9a8ea25f3bc613e79c04a9ba7c8e7
codeanddonuts <kkw200@naver.com>
2019-11-12 15:54:05 +0900

    feat: load database settings from yaml properties.

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -3,21 +3,37 @@ package slipp.support.db;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
-    private static final String DB_USERNAME = "sa";
-    private static final String DB_PW = "";
+    private static final String DB_PROPERTIES_PATH = "/db.properties";
+    private static final String DB_DRIVER = "jdbc.driverClass";
+    private static final String DB_URL = "jdbc.url";
+    private static final String DB_USERNAME = "jdbc.username";
+    private static final String DB_PW = "jdbc.password";
+    private static final String DB_DRIVER_MANUAL = "org.h2.Driver";
+    private static final String DB_URL_MANUAL = "jdbc:h2:mem:jwp-framework";
+    private static final String DB_USERNAME_MANUAL = "sa";
+    private static final String DB_PW_MANUAL = "";
 
     public static DataSource getDataSource() {
-        BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
+        final BasicDataSource ds = new BasicDataSource();
+        try {
+            final Properties properties = new Properties();
+            properties.load(ConnectionManager.class.getResourceAsStream(DB_PROPERTIES_PATH));
+            ds.setDriverClassName(properties.getProperty(DB_DRIVER));
+            ds.setUrl(properties.getProperty(DB_URL));
+            ds.setUsername(properties.getProperty(DB_USERNAME));
+            ds.setPassword(properties.getProperty(DB_PW));
+        } catch (IOException e) {
+            ds.setDriverClassName(DB_DRIVER_MANUAL);
+            ds.setUrl(DB_URL_MANUAL);
+            ds.setUsername(DB_USERNAME_MANUAL);
+            ds.setPassword(DB_PW_MANUAL);
+        }
         return ds;
     }
 
