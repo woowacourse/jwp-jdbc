0c323c529c01cf07925cb33323426337e19202a7
gch01410 <gch01410@naver.com>
2019-11-13 21:10:48 +0900

    refactor: ConnectionManger 에서 properties 파일을 읽도록 수정

--- nextstep-jdbc/src/main/java/nextstep/jdbc/ConnectionManager.java
+++ nextstep-jdbc/src/main/java/nextstep/jdbc/ConnectionManager.java
@@ -1,12 +1,20 @@
 package nextstep.jdbc;
 
+import nextstep.jdbc.exception.PropertyLoadException;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
+    private static final Logger log = LoggerFactory.getLogger(ConnectionManager.class);
+
     private static String driver;
     private static String url;
     private static String userName;
@@ -17,6 +25,21 @@ public class ConnectionManager {
         url = _url;
         userName = _userName;
         password = _password;
+    }
+
+    public static void initialize(String path) {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileReader(path));
+        } catch (IOException e) {
+            log.debug("fail to load property {}", e.getMessage());
+            throw new PropertyLoadException(e);
+        }
+
+        driver = properties.getProperty("jdbc.driverClass");
+        url = properties.getProperty("jdbc.url");
+        userName = properties.getProperty("jdbc.username");
+        password = properties.getProperty("jdbc.password");
     }
 
     public static DataSource getDataSource() {
