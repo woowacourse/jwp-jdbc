e997ea006263df4bfa8d2caa85b546f062f7923b
skygl <jupiter9501@gmail.com>
2019-11-13 22:52:48 +0900

    feat: 사전에 실행시켜야할 테이블 생성 및 세팅 쿼리문 업로드

---
+++ study/src/main/resources/ExecuteThis.sql
@@ -0,0 +1,18 @@
+create table if not exists DevType (DevType varchar(50));
+
+insert into DevType (DevType) values ('Engineering manager');
+insert into DevType (DevType) values ('DevOps specialist');
+insert into DevType (DevType) values ('Desktop or enterprise applications developer');
+insert into DevType (DevType) values ('Embedded applications or devices developer');
+insert into DevType (DevType) values ('Data or business analyst');
+insert into DevType (DevType) values ('System administrator');
+insert into DevType (DevType) values ('Database administrator');
+insert into DevType (DevType) values ('Full-stack developer');
+insert into DevType (DevType) values ('Back-end developer');
+insert into DevType (DevType) values ('Educator or academic researcher');
+insert into DevType (DevType) values ('Designer');
+insert into DevType (DevType) values ('QA or test developer');
+insert into DevType (DevType) values ('Front-end developer');
+insert into DevType (DevType) values ('Data scientist or machine learning specialist');
+insert into DevType (DevType) values ('Mobile developer');
+insert into DevType (DevType) values ('Game or graphics developer');
