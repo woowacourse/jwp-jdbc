cf03ed35d1ce26cef0512b82522319b0df3b329e
hyunsoojung <jhs695@naver.com>
2019-11-14 18:20:17 +0900

    refactor: db.properties 사용

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -1,31 +1,33 @@
 package slipp.support.db;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
-    private static final String DB_USERNAME = "sa";
-    private static final String DB_PW = "";
+    private static final Logger log = LoggerFactory.getLogger(ConnectionManager.class);
+    private static final String DB_DRIVER = "jdbc.driverClass";
+    private static final String DB_URL = "jdbc.url";
+    private static final String DB_USERNAME = "jdbc.username";
+    private static final String DB_PW = "jdbc.password";
 
     public static DataSource getDataSource() {
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
-        return ds;
-    }
-
-    public static Connection getConnection() {
+        Properties properties = new Properties();
         try {
-            return getDataSource().getConnection();
-        } catch (SQLException e) {
-            throw new IllegalStateException(e);
+            properties.load(ConnectionManager.class.getClassLoader().getResourceAsStream("db.properties"));
+        } catch (IOException e) {
+            log.error("err: {}", e.getMessage());
         }
+
+        ds.setDriverClassName(properties.getProperty(DB_DRIVER));
+        ds.setUrl(properties.getProperty(DB_URL));
+        ds.setUsername(properties.getProperty(DB_USERNAME));
+        ds.setPassword(properties.getProperty(DB_PW));
+        return ds;
     }
 }
