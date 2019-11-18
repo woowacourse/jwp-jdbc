8117900fdf9ca2d5055276582793cfa09e7029a0
Park GyeongCheol <star_final@naver.com>
2019-11-18 16:58:12 +0900

    [대용량 데이터 처리] 철시 미션 제출합니다. (#67)
    * feat: update, findAll 구현
    
    * feat: Insert, Update JdbcTemplate
    
    * refactor: JdbcTemplate 템플릿 메서드 패턴 적용
    
    * refactor: Jdbc 라이브러리 리펙토링
    
    * refactor: JdbcTemplate getConnection & try-with-resource
    
    * feat: 가변인자 지원
    
    * refactor: select rowMapper 생성 메서드 추출
    
    * refactor: JdbcTemplate Exception 처리
    
    * refactor: Connection 생성 로직 변경
    - datasource를 JdbcTemplate을 생성할 때 받아서 Connection 생성에 활용하도록 변경
    
    * refactor: JdbcTemplate
    - 사용하는 부분에서 SQL try/catch 처리를 하지 않도록 라이브러리 단에서 처리
    - 여러 클래스에 대응하기 위해 JdbcTemplate 클래스단에서 제네릭 처리를 메서드 단으로 축소
    - setObjects -> setPreparedStatementFor 변경
    
    * refactor: application 컨트롤러 단 인메모리 Database에ㅓ UserDao 변경
    
    * refactor: JdbcTemplate
    - logger 추가로 error 발생시 메세지 로깅
    - exception 처리 메서드화 (validNotOnlyResult, validNoResult)
    
    * refactor: JdbcTemplate PreparedStatement set* 시작 인덱스 상수화
    
    * feat: Properties 파일을 읽어 설정값을 가져오는 Reader 클래스 구현
    - 테스트용 db.properties
    - 다른 설정 파일 형식도 확장성 있게 지원할 수 있도록 PropertyReader 인터페이스 생성
    
    * feat: db.properties 읽어서 설정
    
    * fix: 의존성 추가
    
    * feat: Index 추가 및 정규화 테이블 생성 sql 문
    
    * test: 쿼리 결과 검증 테스트
    
    * refactor: 정규화 테이블 인덱스 추가
    
    * refactor: SELECT 문 최적화
    
    * styles: nomalize.sql -> normalize.sql
    
    * refactor: ConnectionManager.getDataSource
    
    - 설정 property 파일의 경로를 주입받는 getDataSource 추가
    
    * refactor: ConnectionManager.getDataSource 경로 주입
    
    * fix: INDEX 생성 쿼리 DEV_DATA가 생성된 후 실행되도록 변경

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -3,21 +3,43 @@ package slipp.support.db;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
-    private static final String DB_USERNAME = "sa";
-    private static final String DB_PW = "";
+    private static final String DEFAULT_DB_DRIVER = "org.h2.Driver";
+    private static final String DEFAULT_DB_URL = "jdbc:h2:mem:jwp-framework";
+    private static final String DEFAULT_DB_USERNAME = "sa";
+    private static final String DEFAULT_DB_PW = "";
 
     public static DataSource getDataSource() {
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
+        ds.setDriverClassName(DEFAULT_DB_DRIVER);
+        ds.setUrl(DEFAULT_DB_URL);
+        ds.setUsername(DEFAULT_DB_USERNAME);
+        ds.setPassword(DEFAULT_DB_PW);
+
+        return ds;
+    }
+
+    public static DataSource getDataSource(String path) {
+        Properties properties = new Properties();
+        try (InputStream in = new FileInputStream(path)) {
+            properties.load(in);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(properties.getProperty("jdbc.driverClass"));
+        ds.setUrl(properties.getProperty("jdbc.url"));
+        ds.setUsername(properties.getProperty("jdbc.username"));
+        ds.setPassword(properties.getProperty("jdbc.password"));
+
         return ds;
     }
 
---
+++ study/src/test/java/sql/QueryTest.java
@@ -0,0 +1,78 @@
+package sql;
+
+import nextstep.jdbc.JdbcTemplate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import slipp.support.db.ConnectionManager;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QueryTest {
+
+    private static final String TABLE_NAME = "survey_results_public";
+    private static final String DB_CONFIGURATION_PATH = "src/test/resources/db.properties";
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        jdbcTemplate = new JdbcTemplate(ConnectionManager.getDataSource(DB_CONFIGURATION_PATH));
+    }
+
+    @Test
+    void coding_as_a_hobby() {
+        String sql = String.format("SELECT hobby, (count(*) * 100) / (SELECT count(*) FROM %s) as ratio FROM %s GROUP BY hobby", TABLE_NAME, TABLE_NAME);
+
+        List<QueryResult<Double>> queryResults = jdbcTemplate.query(sql, rs -> new QueryResult<>(
+                rs.getString("hobby"), rs.getDouble("ratio")
+        ));
+
+        Map<String, Double> result = convertResultToMap(queryResults);
+        assertThat(result.get("No")).isEqualTo(19.1776);
+        assertThat(result.get("Yes")).isEqualTo(80.8224);
+    }
+
+    @Test
+    void years_of_Professional_Coding_Experience_by_Developer_Type() {
+        int typeSize = 16;
+        List<String> developerTypes = Arrays.asList(
+                "Engineering manager", "DevOps specialist", "Desktop or enterprise applications developer",
+                "Embedded applications or devices developer", "Data or business analyst", "System administrator",
+                "Database administrator", "Full-stack developer", "Back-end developer",
+                "Educator or academic researcher", "Designer", "QA or test developer",
+                "Front-end developer", "Data scientist or machine learning specialist",
+                "Mobile developer", "Game or graphics developer"
+        );
+
+        List<Double> professionalYears = Arrays.asList(
+                10.2, 8.0, 7.7, 7.5, 7.2, 7.0, 6.9, 6.3,
+                6.2, 6.2, 6.0, 5.8, 5.5, 5.5, 5.2, 4.6
+        );
+
+        String normalizedTable = "DEV_DATA";
+        String sql = String.format("SELECT Dev_type as devType, round(avg(year), 1) as result\n" +
+                "FROM %s as d\n" +
+                "WHERE Dev_type <> \"NA\"\n" +
+                "group by Dev_type\n" +
+                "order by result DESC;", normalizedTable);
+
+        List<QueryResult<Double>> queryResults = jdbcTemplate.query(sql, rs -> new QueryResult<>(
+                rs.getString("devType"), rs.getDouble("result")
+        ));
+
+        Map<String, Double> result = convertResultToMap(queryResults);
+
+        for (int i = 0; i < typeSize; i++) {
+            assertThat(result.get(developerTypes.get(i))).isEqualTo(professionalYears.get(i));
+        }
+    }
+
+    private <T> Map<String, T> convertResultToMap(List<QueryResult<T>> queryResults) {
+        return queryResults.stream()
+                .collect(Collectors.toMap(QueryResult::getName, QueryResult::getResult));
+    }
+}
