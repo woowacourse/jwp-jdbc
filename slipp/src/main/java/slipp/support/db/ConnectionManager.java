f3cceaac481a9a00e17acc3a955273fc06726ca6
Seo ohsang <ss1511@naver.com>
2019-11-18 18:37:58 +0900

    [대용량 데이터 처리] 션 미션 제출합니다. (#81)
    * feat: UserDao#update, findAll 구현
    
    * refactor: UserDao에 try-with-resources문 적용
    
    * refactor: JdbcTemplate#executeQuery 추가
    
    * refactor: JdbcTemplateException 추가
    
    * refactor: UserDao#update 리팩토링
    
    * refactor: UserDao#findAll 리팩토링
    
    * refactor: UserDao#findByUserId 리팩토링
    
    * refactor: RowMapper 추가
    
    * refactor: RowMapper를 이용한 JdbcTemplate#query 추가
    
    * refactor: UserDao#insert 메소드 쿼리 개선
    
    * refactor: UserDao를 LazyHolder를 이용한 Singleton으로 변경
    
    * refactor: Controller에 UserDao 적용
    
    * feat: JdbcTemplate에 트랜잭션 기능 추가
    
    * refactor: JdbcTemplate의 제네릭 강제형변환 제거
    
    * refactor: JdbcTemplate의 Connection 설정 수정
    
    * refactor: Reflection방식의 JdbcTemplate 메소드 삭제
    
    * refactor: JdbcTemplate#queryForObject 메소드가 Optional을 반환하도록 수정, 관련 컨트롤러 로직 수정
    
    * refactor: JdbcTemplate에서 트랜잭션 관리 코드 삭제
    
    * refactor: PreparedStatementSetter 인터페이스 추가
    
    * chore: 메소드 파라미터 순서 변경
    
    * chore : code formatting
    
    * refactor: ConnectionManager에서 db설정 분리
    
    * feat: Query조회를 위한 study모듈에 DataSetTest 추가
    
    * refactor: JdbcTemplate에 가변인자 방식의 메소드 오버로딩
    
    * test: Hobby 테스트 추가
    
    * test: CodingYears 테스트 추가

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -3,22 +3,39 @@ package slipp.support.db;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:mem:jwp-framework";
-    private static final String DB_USERNAME = "sa";
-    private static final String DB_PW = "";
+
+    public static final String DEFAULT_PROPERTIES_PATH = "src/main/resources/db.properties";
 
     public static DataSource getDataSource() {
+        return getDataSource(DEFAULT_PROPERTIES_PATH);
+    }
+
+    public static DataSource getDataSource(String directory) {
+        Properties properties = readProperties(directory);
         BasicDataSource ds = new BasicDataSource();
-        ds.setDriverClassName(DB_DRIVER);
-        ds.setUrl(DB_URL);
-        ds.setUsername(DB_USERNAME);
-        ds.setPassword(DB_PW);
+        ds.setDriverClassName(properties.getProperty("jdbc.driverClass"));
+        ds.setUrl(properties.getProperty("jdbc.url"));
+        ds.setUsername(properties.getProperty("jdbc.username"));
+        ds.setPassword(properties.getProperty("jdbc.password"));
         return ds;
+    }
+
+    private static Properties readProperties(String directory) {
+        Properties properties = new Properties();
+        String resource = directory;
+        try {
+            properties.load(new FileReader(resource));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties;
     }
 
     public static Connection getConnection() {
