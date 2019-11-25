747c4ff14f0676157b424cc9e81320a6f4451936
김민준 <starkim06@naver.com>
2019-11-25 17:40:45 +0900

    [대용량 데이터 처리] 뚱이 미션 제출합니다. (#84)
    * feat: db.properties 파일 추가 및 TEST파일 추가
    
    * refactor: 쿼리 수정
    
    * fix: 쿼리 수정
    
    * refactor: 주석 제거

--- slipp/src/main/java/slipp/support/db/ConnectionManager.java
+++ slipp/src/main/java/slipp/support/db/ConnectionManager.java
@@ -3,8 +3,12 @@ package slipp.support.db;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
     private static final String DB_DRIVER = "org.h2.Driver";
@@ -18,6 +22,33 @@ public class ConnectionManager {
         ds.setUrl(DB_URL);
         ds.setUsername(DB_USERNAME);
         ds.setPassword(DB_PW);
+        return ds;
+    }
+
+    public static DataSource getDataSource(String path) {
+        Properties properties = new Properties();
+        BasicDataSource ds = new BasicDataSource();
+        FileReader reader = null;
+        try {
+            reader = new FileReader(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            properties.load(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String driverClass = properties.getProperty("jdbc.driverClass");
+        String url = properties.getProperty("jdbc.url");
+        String userName = properties.getProperty("jdbc.username");
+        String password = properties.getProperty("jdbc.password");
+
+        ds.setDriverClassName(driverClass);
+        ds.setUrl(url);
+        ds.setUsername(userName);
+        ds.setPassword(password);
+
         return ds;
     }
 
